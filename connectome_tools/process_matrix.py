c05d63f3d63831110a7076833f5e82cd1d4a34cd
Michael Winding <mwinding@Michaels-MBP.lan>
2020-02-19 17:30:33 +0000

    implemented row summing based on cell type of interest

--- connectome_tools/process_matrix.py
+++ connectome_tools/process_matrix.py
@@ -3,6 +3,17 @@
 import pandas as pd
 import numpy as np
 
+# trim out neurons not currently in the brain matrix
+def trim_missing(skidList, brainMatrix):
+    trimmedList = []
+    for i in skidList:
+        if(i in brainMatrix.index):
+            trimmedList.append(i)
+        else:
+            print("*WARNING* skid: %i is not in whole brain matrix" % (i))
+    
+    return(trimmedList)
+
 # identify skeleton ID of hemilateral neuron pair, based on CSV pair list
 def identify_pair(skid, pairList):
     
@@ -16,7 +27,7 @@ def identify_pair(skid, pairList):
 
 # returns paired skids in array [left, right]; can input either left or right skid of a pair to identify
 def get_paired_skids(skid, pairList):
-     
+
     if(skid in pairList["leftid"].values):
         pair_right = pairList["rightid"][pairList["leftid"]==skid].iloc[0]
         pair_left = skid
@@ -25,11 +36,27 @@ def get_paired_skids(skid, pairList):
         pair_left = pairList["leftid"][pairList["rightid"]==skid].iloc[0]
         pair_right = skid
 
+    if((skid in pairList["leftid"].values) == False and (skid in pairList["rightid"].values) == False):
+        print("skid %i is not in paired list" % (skid))
+        return(0)
+
     return([pair_left, pair_right])
 
 # converts array of skids into left-right pairs in separate columns
-def extract_pairs_from_list(filepath):
-    return()
+def extract_pairs_from_list(skids, pairList):
+    pairs = []
+    for i in skids:
+        if(int(i) in pairList["leftid"].values):
+            pair = get_paired_skids(int(i), pairList)
+            pairs.append({'leftid': pair[0], 'rightid': pair[1]})
+
+        # delaying with non-paired neurons
+        # UNTESTED
+        if (pair==0):
+            break
+
+    pairs = pd.DataFrame(pairs)
+    return(pairs)
 
 # converts a interlaced left-right pair adjacency matrix into a binary connection matrix based on some threshold
 def binary_matrix(matrix_path, threshold): 
@@ -55,7 +82,7 @@ def binary_matrix(matrix_path, threshold):
 
 # compares neuron similarity based on inputs, outputs, or both
 # outputs a matrix where each row/column is a pair of neurons
-# todo
+# NOT currently working
 def similarity_matrix(matrix_path, type):
     matrix = pd.read_csv(matrix_path, header=0, index_col=0, quotechar='"', skipinitialspace=True)
 
@@ -67,3 +94,26 @@ def similarity_matrix(matrix_path, type):
     sim_matrix = pd.DataFrame(sim_matrix, columns = matrix.columns[oddCols], index = matrix.index[oddRows])
 
     return(sim_matrix)
+
+# summing input from a group of upstream neurons
+# generating DataFrame with sorted leftid, rightid, summed-input left, summed-input right
+def summed_input(group_skids, matrix, pairList):
+    submatrix = matrix.loc[group_skids, :]
+    submatrix = submatrix.sum(axis = 0)
+
+    cols = ['leftid', 'rightid', 'leftid_input', 'rightid_input']
+    summed_paired = []
+
+    for i in range(0, len(pairList['leftid'])):
+        if(str(pairList['leftid'][i]) in submatrix.index):
+            left_identifier = str(pairList['leftid'][i])
+            left_sum = submatrix.loc[left_identifier]
+        
+            right_identifier = str(identify_pair(pairList['leftid'][i], pairList))
+            right_sum = submatrix.loc[right_identifier]
+                
+            summed_paired.append([left_identifier, right_identifier, left_sum, right_sum])
+
+    summed_paired = pd.DataFrame(summed_paired, columns= cols)
+
+    return(summed_paired)
--- identifyLHNs_CNs.py
+++ identifyLHNs_CNs.py
@@ -2,6 +2,9 @@ import connectome_tools.process_matrix as promat
 import pandas as pd
 import glob as gl
 
+# load whole brain connectivity matrix
+matrix = pd.read_csv('data/Gadn-pair-sorted.csv', header=0, index_col=0)
+
 # import pair list CSV, manually generated
 pairs = pd.read_csv('data/bp-pairs-2020-01-28.csv', header = 0)
 
@@ -15,8 +18,17 @@ tPNs = pd.read_json(neuron_groups[2])['skeleton_id'].values
 uPNs = pd.read_json(neuron_groups[3])['skeleton_id'].values
 vPNs = pd.read_json(neuron_groups[4])['skeleton_id'].values
 
-# sort each list into paired neurons
+# trim out neurons that aren't in the matrix (to avoid code breaking)
+mPNs = promat.trim_missing(mPNs, matrix)
+tPNs = promat.trim_missing(tPNs, matrix)
+uPNs = promat.trim_missing(uPNs, matrix)
+vPNs = promat.trim_missing(vPNs, matrix)
+MBONs = promat.trim_missing(MBONs, matrix)
 
-
-#binaryBrain = promat.binary_matrix('data/Gadn-pair-sorted.csv', 0.01)
+# generating whole brain matrices with summed input from selected neuron type
+sum_vPNs = promat.summed_input(vPNs, matrix, pairs)
+sum_mPNs = promat.summed_input(mPNs, matrix, pairs)
+sum_tPNs = promat.summed_input(tPNs, matrix, pairs)
+sum_uPNs = promat.summed_input(uPNs, matrix, pairs)
+sum_MBONs = promat.summed_input(MBONs, matrix, pairs)
 
