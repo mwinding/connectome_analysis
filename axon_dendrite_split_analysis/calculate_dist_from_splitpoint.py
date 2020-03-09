1c7620126b01308a8ac5e3a1c3628e11dfeb331a
Michael Winding <mwinding@alumni.nd.edu>
2020-03-09 14:54:51 +0000

    completed dist from splitpoint script

--- axon_dendrite_split_analysis/calculate_dist_from_splitpoint.py
+++ axon_dendrite_split_analysis/calculate_dist_from_splitpoint.py
@@ -8,13 +8,12 @@ import numpy as np
 import networkx as nx 
 
 
-connectors = pd.read_csv('axon_dendrite_data/splittable_connectors_all.csv', header=0, skipinitialspace=True, keep_default_na = False)
-skeletons = pd.read_csv('axon_dendrite_data/splittable_skeletons_all.csv', header=0, skipinitialspace=True, keep_default_na = False)
+connectors = pd.read_csv('axon_dendrite_data/splittable_connectors_all_mod.csv', header=0, skipinitialspace=True, keep_default_na = False)
+skeletons = pd.read_csv('axon_dendrite_data/splittable_skeletons_all_mod.csv', header=0, skipinitialspace=True, keep_default_na = False)
 splits = pd.read_csv('axon_dendrite_data/brain_acc_split_nodes.csv', header=0, skipinitialspace=True, keep_default_na = False)
 
 list_skeletons = proskel.split_skeleton_lists(skeletons)
 list_connectors = proskel.split_skeleton_lists(connectors)
-
 
  # convert each skeleton morphology CSV into networkx graph
 skeleton_graphs = []
@@ -26,7 +25,6 @@ for i in tqdm(range(len(list_skeletons))):
 roots = []
 for i in tqdm(range(len(skeleton_graphs))):
     root = proskel.identify_root(skeleton_graphs[i])
-    #print("skeleton %i has root %i" %(skeleton[], root))
     roots.append(root)
 
 # identify split points
@@ -34,7 +32,17 @@ skids = np.unique(skeletons['skeleton_id'])
 
 split_ids = []
 for i in tqdm(range(len(skids))):
-    split_ids.append(splits['treenode'][splits['skeleton'] == skids[i]].values[0])
+    if(sum(splits['skeleton'] == skids[i])>=1):
+        split_ids.append(splits['treenode'][splits['skeleton'] == skids[i]].values[0])
+    if(sum(splits['skeleton'] == skids[i])==0):
+        print('skid %i has no split point' % skids[i])
+
+
+print(len(skeleton_graphs))
+print(len(list_connectors))
+print(len(roots))
+print(len(split_ids))
+
 
 # calculate distances of each connector to split for each separate graph
 connectdists_list = []
--- axon_dendrite_split_analysis/remove_skid_from_csv.py
+++ axon_dendrite_split_analysis/remove_skid_from_csv.py
@@ -1,21 +1,28 @@
 import csv
 import pandas as pd
 
-skeletons = pd.read_csv('axon_dendrite_data/splittable_skeletons_left2.csv', header=0, skipinitialspace=True, keep_default_na = False)
-connectors = pd.read_csv('axon_dendrite_data/splittable_connectors_left2.csv', header=0, skipinitialspace=True, keep_default_na = False)
+skeletons = pd.read_csv('axon_dendrite_data/splittable_skeletons_all.csv', header=0, skipinitialspace=True, keep_default_na = False)
+connectors = pd.read_csv('axon_dendrite_data/splittable_connectors_all.csv', header=0, skipinitialspace=True, keep_default_na = False)
 
-#print(len(skeletons))
-#print(len(skeletons.drop('4663875')))
 
-print(sum(skeletons['skeleton_id']!=4663875))
+
 print(len(skeletons))
 #4663875
 
 skeletons = skeletons[skeletons['skeleton_id']!=4663875]
+skeletons = skeletons[skeletons['skeleton_id']!=17360014]
+skeletons = skeletons[skeletons['skeleton_id']!=16595001]
+skeletons = skeletons[skeletons['skeleton_id']!=16675987]
+
+print(len(skeletons))
+
 
 print(len(connectors))
 connectors = connectors[connectors['skeleton_id']!=4663875]
+connectors = connectors[connectors['skeleton_id']!=17360014]
+connectors = connectors[connectors['skeleton_id']!=16595001]
+connectors = connectors[connectors['skeleton_id']!=16675987]
 print(len(connectors))
 
-skeletons.to_csv('axon_dendrite_data/splittable_skeletons_left2_mod.csv')
-connectors.to_csv('axon_dendrite_data/splittable_connectors_left2_mod.csv')
+skeletons.to_csv('axon_dendrite_data/splittable_skeletons_all_mod.csv')
+connectors.to_csv('axon_dendrite_data/splittable_connectors_all_mod.csv')
