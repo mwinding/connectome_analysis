336e9aae07ad76c62601c580a345e4012a69ca33
Michael Winding <mwinding@alumni.nd.edu>
2020-04-06 13:39:02 +0100

    misc changes

--- synapse_distributions/pymaid_test.py
+++ synapse_distributions/pymaid_test.py
@@ -8,10 +8,12 @@ from pymaid_creds import url, name, password, token
 #pymaid.clear_cache()
 rm = pymaid.CatmaidInstance(url, name, password, token)
 
-annotated = pymaid.get_annotated('test_meta')
-test1 = annotated['name'][0]
-test2 = annotated['name'][1]
+annotated = pymaid.get_annotated('mw neuron groups')
+test1 = annotated['name'][3]
+test2 = annotated['name'][4]
 
 print(annotated)
 print(pymaid.get_skids_by_annotation(test1))
 print(pymaid.get_skids_by_annotation(test2))
+
+
