e86296114634fe054f2344b8da610fc69b721c26
mwinding <mwinding@alumni.nd.edu>
2020-07-07 17:57:46 +0100

    added multimodal integration plots

--- cascades/cluster_cascades/sensory_cascades_through_clusters.py
+++ cascades/cluster_cascades/sensory_cascades_through_clusters.py
@@ -32,6 +32,14 @@ mg.calculate_degrees(inplace=True)
 adj = mg.adj  # adjacency matrix from the "mg" object
 
 clusters = pd.read_csv('cascades/data/meta-method=color_iso-d=8-bic_ratio=0.95-min_split=32.csv', index_col = 0, header = 0)
+order = pd.read_csv('cascades/data/signal_flow_order_lvl7.csv').values
+
+# make array from list of lists
+order_delisted = []
+for sublist in order:
+    order_delisted.append(sublist[0])
+
+order = np.array(order_delisted)
 
 #%%
 # pull sensory annotations and then pull associated skids
@@ -42,6 +50,12 @@ output_names = pymaid.get_annotated('mw brain outputs').name
 output_skids_list = list(map(pymaid.get_skids_by_annotation, pymaid.get_annotated('mw brain outputs').name))
 output_skids = [val for sublist in output_skids_list for val in sublist]
 
+# order names and skids in desired way for the rest of analysis
+sensory_order = [0, 3, 4, 1, 2, 6, 5]
+input_names_format = ['ORN', 'thermo', 'photo', 'AN', 'MN', 'vtd', 'A00c']
+input_names_format_reordered = [input_names_format[i] for i in sensory_order]
+input_skids_list_reordered = [input_skids_list[i] for i in sensory_order]
+
 #%%
 # cascades from each sensory modality
 from src.traverse import Cascade, to_transmission_matrix
@@ -50,7 +64,7 @@ from src.visualization import matrixplot
 
 # convert skids to indices
 input_indices_list = []
-for input_skids in input_skids_list:
+for input_skids in input_skids_list_reordered:
     indices = np.where([x in input_skids for x in mg.meta.index])[0]
     input_indices_list.append(indices)
 
@@ -126,22 +140,84 @@ for input_hit_hist in input_hit_hist_lvl7:
 # plotting signal flow through clusters
 
 fig, axs = plt.subplots(
-    1, 1, figsize=(10, 10)
+    4, 2, figsize=(10, 10)
 )
 
 fig.tight_layout(pad=2.0)
 
 threshold = 25
+vmax = n_init*.8
 
+for i in range(0, len(input_names_format_reordered)):
+    if(i<4):
+        ax = axs[i, 0]
+    if(i>=4):
+        ax = axs[i-4,1]
+    
+    sns.heatmap(summed_hist_lvl7[i].loc[order], ax = ax, rasterized=True, vmax = vmax)
+    ax.set_ylabel('Individual Clusters')
+    ax.set_yticks([])
+
+    ax.set_xlabel('Hops from %s signal' %input_names_format_reordered[i])
+
+    #sns.heatmap(summed_hist_lvl7[1].loc[sort], ax = ax, rasterized=True)
+
+ax = axs[3, 1]
+ax.axis("off")
+caption = f"Figure: Hop histogram of individual level 7 clusters\nCascades starting at each sensory modality\nending at brain output neurons"
+ax.text(0, 1, caption, va="top")
+
+plt.savefig('cascades/cluster_plots/sensory_through_clusters_lvl7.pdf', format='pdf', bbox_inches='tight')
+
+'''
+# sort based on visit numbers per hop
 sort_template = summed_hist_lvl7[0]
 sort_template = sort_template[sort_template>threshold]
 sort_template.fillna(0)
 sort = sort_template.sort_values(by = list(sort_template.columns), ascending = False).index
-
-ax = axs
-sns.heatmap(summed_hist_lvl7[0].loc[sort], ax = ax, rasterized=True)
-#sns.heatmap(summed_hist_lvl7[1].loc[sort], ax = ax, rasterized=True)
-
-plt.savefig('cascades/cluster_plots/ORN_through_clusters_lvl7.pdf', format='pdf', bbox_inches='tight')
+'''
 
 # %%
+# mutlisensory nature of each cluster
+
+collapsed_hops_lvl7_list = []
+for hist in summed_hist_lvl7:
+    collapsed_hops_lvl7_list.append(hist.sum(axis = 1))
+
+collapsed_hops_lvl7 = pd.DataFrame(collapsed_hops_lvl7_list, index = input_names_format_reordered).T
+
+fg = sns.clustermap(collapsed_hops_lvl7.loc[order], col_cluster = False, yticklabels=False, rasterized = True)
+ax = fg.ax_heatmap
+ax.set_ylabel('Individual Clusters')
+fg.savefig('cascades/cluster_plots/multimodal_nature_of_clusters_lvl7.pdf', format='pdf', bbox_inches='tight')
+
+# checking where inputs, outputs, etc are located in these reordered clusters
+fg.dendrogram_row.reordered_ind
+dendrogram_order = order[fg.dendrogram_row.reordered_ind]
+lvl7 = list(clusters.groupby('lvl7_labels'))
+
+cluster_membership = []
+for cluster_key in dendrogram_order:
+    cluster_temp = clusters[clusters.lvl7_labels==cluster_key]
+    cluster_dSEZ = sum(cluster_temp.dSEZ == True)
+    cluster_dVNC = sum(cluster_temp.dVNC == True)
+    cluster_RG = sum(cluster_temp.RG == True)
+    cluster_input = sum(cluster_temp.input == True)
+    cluster_output = sum(cluster_temp.output == True)
+    cluster_brain = sum(cluster_temp.brain_neurons == True)
+    cluster_all = len(cluster_temp.index)
+
+    cluster_membership.append(dict({'cluster_key': cluster_key,
+                                'total_neurons': cluster_all, 'brain_neurons': cluster_brain/cluster_all, 
+                                'outputs': cluster_output/cluster_all, 'inputs': cluster_input/cluster_all,
+                                'dVNC': cluster_dVNC/cluster_all, 'dSEZ': cluster_dSEZ/cluster_all, 
+                                'RG': cluster_RG/cluster_all}))
+
+cluster_membership = pd.DataFrame(cluster_membership)
+sns.heatmap(cluster_membership.iloc[:, 3:8])
+
+# %%
+# identifying locations of intersecting signals in individual neurons
+# which clusters and which neurons?
+
+
