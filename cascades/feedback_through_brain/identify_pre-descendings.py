6be5a13a99aa73cb0482b1e842f0f03d09829945
mwinding <mwinding@alumni.nd.edu>
2020-08-04 10:54:32 +0100

    identification script based on 5% input threshold

--- cascades/feedback_through_brain/identify_pre-descendings.py
+++ cascades/feedback_through_brain/identify_pre-descendings.py
@@ -45,10 +45,20 @@ output_skids = [val for sublist in output_skids_list for val in sublist]
 output_names_reordered = [output_names[i] for i in output_order]
 output_skids_list_reordered = [output_skids_list[i] for i in output_order]
 
+def skid_to_index(skid, mg):
+    index_match = np.where(mg.meta.index == skid)[0]
+    if(len(index_match)==1):
+        return(index_match[0])
+    if(len(index_match)!=1):
+        print('Not one match for skid %i!' %skid)
+
 # convert skids to indices
 output_indices_list = []
 for skids in output_skids_list_reordered:
-    indices = np.where([x in skids for x in mg.meta.index])[0]
+    indices = []
+    for skid in skids:
+        index = skid_to_index(skid, mg)
+        indices.append(index)
     output_indices_list.append(indices)
 # %%
 # identify pre-descending types
@@ -123,8 +133,7 @@ for i in range(0, 2):
 
 # %%
 # identify pre-descending types
-# top hits per descending neuron?
-
+# contributing 5% input to a particular descending neuron
 import connectome_tools.process_matrix as promat
 
 def skid_to_index(skid, mg):
@@ -136,7 +145,6 @@ def skid_to_index(skid, mg):
 
 def index_to_skid(index, mg):
     return(mg.meta.iloc[index, :].name)
-
 
 # import pairs
 pairs = pd.read_csv('data/pairs-2020-05-08.csv', header = 0)
@@ -152,6 +160,16 @@ for i in range(0, len(dVNC_pairs)):
     dVNC_pair_order.append(dVNC_pairs.iloc[i].leftid)
     dVNC_pair_order.append(dVNC_pairs.iloc[i].rightid)
 
+dSEZ_pair_order = []
+for i in range(0, len(dSEZ_pairs)):
+    dSEZ_pair_order.append(dSEZ_pairs.iloc[i].leftid)
+    dSEZ_pair_order.append(dSEZ_pairs.iloc[i].rightid)
+
+RG_pair_order = []
+for i in range(0, len(RG_pairs)):
+    RG_pair_order.append(RG_pairs.iloc[i].leftid)
+    RG_pair_order.append(RG_pairs.iloc[i].rightid)
+
 # left_right interlaced order for brain matrix
 brain_pair_order = []
 for i in range(0, len(brain_skids_pairs)):
@@ -159,64 +177,137 @@ for i in range(0, len(brain_skids_pairs)):
     brain_pair_order.append(brain_skids_pairs.iloc[i].rightid)
 
 interlaced_dVNC_mat = dVNC_mat.loc[brain_pair_order, dVNC_pair_order]
-
-
-# summed input onto descending between pairs
-oddCols = np.arange(0, len(interlaced_dVNC_mat.columns), 2)
-oddRows = np.arange(0, len(interlaced_dVNC_mat.index), 2)
-
-sumMat = np.zeros(shape=(len(oddRows),len(oddCols)))
-sumMat = pd.DataFrame(sumMat, columns = interlaced_dVNC_mat.columns[oddCols], 
-                            index = interlaced_dVNC_mat.index[oddRows])
-
-for i_iter, i in enumerate(oddRows):
-    for j_iter, j in enumerate(oddCols):
-        summed_pairs = interlaced_dVNC_mat.iat[i, j] + interlaced_dVNC_mat.iat[i+1, j+1] + interlaced_dVNC_mat.iat[i+1, j] + interlaced_dVNC_mat.iat[i, j+1]
-        sumMat.iat[i_iter, j_iter] = summed_pairs
+interlaced_dSEZ_mat = dSEZ_mat.loc[brain_pair_order, dSEZ_pair_order]
+interlaced_RG_mat = RG_mat.loc[brain_pair_order, RG_pair_order]
 
 # convert to %input of descendings' dendrite
 for column in interlaced_dVNC_mat.columns:
     dendrite_input = mg.meta.loc[column].dendrite_input
     interlaced_dVNC_mat.loc[:, column] = interlaced_dVNC_mat.loc[:, column]/dendrite_input
-# doesn't seem to work
-'''
-# converts a interlaced left-right pair adjacency matrix into a binary connection matrix based on some threshold
-def binary_matrix(data, threshold, total_threshold): 
 
-    oddCols = np.arange(0, len(data.columns), 2)
-    oddRows = np.arange(0, len(data.index), 2)
+for column in interlaced_dSEZ_mat.columns:
+    dendrite_input = mg.meta.loc[column].dendrite_input
+    interlaced_dSEZ_mat.loc[:, column] = interlaced_dSEZ_mat.loc[:, column]/dendrite_input
 
-    # column names are the skid of left neuron from pair
-    binMat = np.zeros(shape=(len(oddRows),len(oddCols)))
-    binMat = pd.DataFrame(binMat, columns = data.columns[oddCols], index = data.index[oddRows])
+for column in interlaced_RG_mat.columns:
+    dendrite_input = mg.meta.loc[column].dendrite_input
+    interlaced_RG_mat.loc[:, column] = interlaced_RG_mat.loc[:, column]/dendrite_input
 
-    for i in oddRows:
-        for j in oddCols:
-            sum_all = data.iat[i, j] + data.iat[i+1, j+1] + data.iat[i+1, j] + data.iat[i, j+1]
-            if(data.iat[i, j] >= threshold and data.iat[i+1, j+1] >= threshold and sum_all >= total_threshold):
-                binMat.iat[int(i/2), int(j/2)] = 1
+# summed input onto descending between pairs
+oddCols_dVNC = np.arange(0, len(interlaced_dVNC_mat.columns), 2)
+oddCols_dSEZ = np.arange(0, len(interlaced_dSEZ_mat.columns), 2)
+oddCols_RG = np.arange(0, len(interlaced_RG_mat.columns), 2)
 
-            if(data.iat[i+1, j] >= threshold and data.iat[i, j+1] >= threshold and sum_all >= total_threshold):
-                binMat.iat[int(i/2), int(j/2)] = 1
-        
-    return(binMat)
+oddRows = np.arange(0, len(interlaced_dVNC_mat.index), 2)
 
-dVNC_connections = binary_matrix(interlaced_dVNC_mat, threshold = 3, total_threshold = 6)
-'''
+# initializing summed matrices for each descending type
+sumMat_dVNC = np.zeros(shape=(len(oddRows),len(oddCols_dVNC)))
+sumMat_dVNC = pd.DataFrame(sumMat_dVNC, columns = interlaced_dVNC_mat.columns[oddCols_dVNC], index = interlaced_dVNC_mat.index[oddRows])
 
-# add columns for adj indices
-'''
-indices_left = []
-indices_right = []
-for i in range(0, len(dVNC_pairs)):
-    index_left = skid_to_index(dVNC_pairs.loc[i, 'leftid'], mg)
-    index_right = skid_to_index(dVNC_pairs.loc[i, 'rightid'], mg)
-    indices_left.append(index_left)
-    indices_right.append(index_right)
+sumMat_dSEZ = np.zeros(shape=(len(oddRows),len(oddCols_dSEZ)))
+sumMat_dSEZ = pd.DataFrame(sumMat_dVNC, columns = interlaced_dSEZ_mat.columns[oddCols_dSEZ], index = interlaced_dSEZ_mat.index[oddRows])
 
-dVNC_pairs['left_index'] = indices_left
-dVNC_pairs['right_index'] = indices_right
-'''
+sumMat_RG = np.zeros(shape=(len(oddRows),len(oddCols_RG)))
+sumMat_RG = pd.DataFrame(sumMat_RG, columns = interlaced_RG_mat.columns[oddCols_RG], index = interlaced_RG_mat.index[oddRows])
 
+for i_iter, i in enumerate(oddRows):
+    for j_iter, j in enumerate(oddCols_dVNC):
+        summed_pairs = interlaced_dVNC_mat.iat[i, j] + interlaced_dVNC_mat.iat[i+1, j+1] + interlaced_dVNC_mat.iat[i+1, j] + interlaced_dVNC_mat.iat[i, j+1]
+        sumMat_dVNC.iat[i_iter, j_iter] = summed_pairs/2
+
+for i_iter, i in enumerate(oddRows):
+    for j_iter, j in enumerate(oddCols_dSEZ):
+        summed_pairs = interlaced_dSEZ_mat.iat[i, j] + interlaced_dSEZ_mat.iat[i+1, j+1] + interlaced_dSEZ_mat.iat[i+1, j] + interlaced_dSEZ_mat.iat[i, j+1]
+        sumMat_dSEZ.iat[i_iter, j_iter] = summed_pairs/2
+
+for i_iter, i in enumerate(oddRows):
+    for j_iter, j in enumerate(oddCols_RG):
+        summed_pairs = interlaced_RG_mat.iat[i, j] + interlaced_RG_mat.iat[i+1, j+1] + interlaced_RG_mat.iat[i+1, j] + interlaced_RG_mat.iat[i, j+1]
+        sumMat_RG.iat[i_iter, j_iter] = summed_pairs/2
+# %%
+# plotting number of connections to and from descendings
+fig, axs = plt.subplots(
+    3, 2, figsize=(8, 8)
+)
+
+fig.tight_layout(pad = 2.5)
+threshold = 0.05 # average 5% input threshold
+binwidth = 1
+x_range = list(range(0, 11))
+align = 'left'
+
+ax = axs[0, 0]
+count_per_us_neuron = (sumMat_dVNC.values>threshold).sum(axis=1)
+data = count_per_us_neuron[count_per_us_neuron>0]
+ax.hist(data, bins=range(min(data), max(data) + binwidth, binwidth), align = align)
+ax.set_ylabel('Number of brain neuron pairs')
+ax.set_xlabel('Connection(s) to dVNCs')
+ax.set_xticks(x_range)
+
+ax = axs[0, 1]
+count_per_descending = (sumMat_dVNC.values>threshold).sum(axis=0)
+data = count_per_descending
+ax.hist(data, bins=range(min(data), max(data) + binwidth, binwidth), align = align)
+ax.set_ylabel('Number of dVNC neuron pairs')
+ax.set_xlabel('Connection(s) received')
+ax.set_xticks(x_range)
+
+ax = axs[1, 0]
+count_per_us_neuron = (sumMat_dSEZ.values>threshold).sum(axis=1)
+data = count_per_us_neuron[count_per_us_neuron>0]
+ax.hist(data, bins=range(min(data), max(data) + binwidth, binwidth), align = align)
+ax.set_ylabel('Number of brain neuron pairs')
+ax.set_xlabel('Connection(s) to dSEZs')
+ax.set_xticks(x_range)
+
+ax = axs[1, 1]
+count_per_descending = (sumMat_dSEZ.values>threshold).sum(axis=0)
+data = count_per_descending
+ax.hist(data, bins=range(min(data), max(data) + binwidth, binwidth), align = align)
+ax.set_ylabel('Number of dSEZ neuron pairs')
+ax.set_xlabel('Connection(s) received')
+ax.set_xticks(x_range)
+
+ax = axs[2, 0]
+count_per_us_neuron = (sumMat_RG.values>threshold).sum(axis=1)
+data = count_per_us_neuron[count_per_us_neuron>0]
+ax.hist(data, bins=range(min(data), max(data) + binwidth, binwidth), align = align)
+ax.set_ylabel('Number of brain neuron pairs')
+ax.set_xlabel('Connection(s) to RGNs')
+ax.set_xticks(x_range)
+
+ax = axs[2, 1]
+count_per_descending = (sumMat_RG.values>threshold).sum(axis=0)
+data = count_per_descending
+ax.hist(data, bins=range(min(data), max(data) + binwidth, binwidth), align = align)
+ax.set_ylabel('Number of RGN pairs')
+ax.set_xlabel('Connection(s) received')
+ax.set_xticks(x_range)
+
+plt.savefig('cascades/feedback_through_brain/plots/connections_from_to_descendings_5percent_threshold.pdf', bbox_inches='tight', transparent = True)
+# %%
+# export pre-descending neurons
+pre_dVNC = (sumMat_dVNC.values>threshold).sum(axis=1)>0
+pre_dSEZ = (sumMat_dSEZ.values>threshold).sum(axis=1)>0
+pre_RGN = (sumMat_RG.values>threshold).sum(axis=1)>0
+
+# identify indices == True, i.e. left skids of pairs that are pre-descendings
+indices_pre_dVNC = np.where(pre_dVNC)[0]
+indices_pre_dSEZ = np.where(pre_dSEZ)[0]
+indices_pre_RGN = np.where(pre_RGN)[0]
+
+pre_dVNC_skidleft = [sumMat_dVNC.index[x] for x in indices_pre_dVNC]
+pre_dSEZ_skidleft = [sumMat_dSEZ.index[x] for x in indices_pre_dSEZ]
+pre_RGN_skidleft = [sumMat_RG.index[x] for x in indices_pre_RGN]
+
+# select pre-descending pair skids
+brain_skids_pairs.index = brain_skids_pairs.leftid
+pre_dVNC_skids = brain_skids_pairs.loc[pre_dVNC_skidleft, :]
+pre_dSEZ_skids = brain_skids_pairs.loc[pre_dSEZ_skidleft, :]
+pre_RGN_skids = brain_skids_pairs.loc[pre_RGN_skidleft, :]
+
+pre_dVNC_skids.to_csv('cascades/feedback_through_brain/plots/pre_dVNC_skids.csv', index = False)
+pre_dSEZ_skids.to_csv('cascades/feedback_through_brain/plots/pre_dSEZ_skids.csv', index = False)
+pre_RGN_skids.to_csv('cascades/feedback_through_brain/plots/pre_RGN_skids.csv', index = False)
 
 # %%
