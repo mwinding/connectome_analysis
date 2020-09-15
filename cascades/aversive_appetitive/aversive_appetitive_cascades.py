d0ea686dce7c377237c8c7f40a0364c75678df20
mwinding <mwinding@alumni.nd.edu>
2020-09-15 10:20:59 +0100

    exploratory analysis

--- cascades/aversive_appetitive/aversive_appetitive_cascades.py
+++ cascades/aversive_appetitive/aversive_appetitive_cascades.py
@@ -57,8 +57,8 @@ order = list(order_df.cluster)
 #%%
 # pull sensory annotations and then pull associated skids
 
-aversive_skids_list = list(map(pymaid.get_skids_by_annotation, ['mw MBON subclass_aversive', 'mw thermosensories', 'mw photoreceptors', 'mw A00c']))
-appetitive_skids_list = list(map(pymaid.get_skids_by_annotation, ['mw MBON subclass_appetitive', 'mw ORN']))
+aversive_skids_list = list(map(pymaid.get_skids_by_annotation, ['mw MBON subclass_aversive', 'mw thermosensories', 'mw photoreceptors', 'mw A00c', 'mw ORN aversive published']))
+appetitive_skids_list = list(map(pymaid.get_skids_by_annotation, ['mw MBON subclass_appetitive', 'mw ORN appetitive published', 'CN-33']))
 
 aversive_skids = [val for sublist in aversive_skids_list for val in sublist]
 appetitive_skids = [val for sublist in appetitive_skids_list for val in sublist]
@@ -69,7 +69,6 @@ output_skids = [val for sublist in output_skids_list for val in sublist]
 
 #%%
 # cascades from each output type, ending at brain inputs 
-# maybe should switch to senosry second-order?
 from src.traverse import Cascade, to_transmission_matrix
 from src.traverse import to_markov_matrix, RandomWalk
 from src.traverse import TraverseDispatcher
@@ -84,9 +83,13 @@ aversive_indices = np.where([x in aversive_skids for x in mg.meta.index])[0]
 appetitive_indices = np.where([x in appetitive_skids for x in mg.meta.index])[0]
 all_output_indices = np.where([x in output_skids for x in mg.meta.index])[0]
 
+# add KCs to stop_nodes because they remove the valence signal
+KC_skids = pymaid.get_skids_by_annotation('mw KC')
+KC_indices = np.where([x in KC_skids for x in mg.meta.index])[0]
+
 p = 0.05
 max_hops = 10
-n_init = 100
+n_init = 1000
 simultaneous = True
 transition_probs = to_transmission_matrix(adj, p)
 
@@ -94,7 +97,7 @@ transition_probs = to_transmission_matrix(adj, p)
 cdispatch = TraverseDispatcher(
     Cascade,
     transition_probs,
-    stop_nodes = all_output_indices,
+    stop_nodes = np.concatenate([KC_indices, all_output_indices]),
     max_hops=max_hops,
     allow_loops = False,
     n_init=n_init,
@@ -159,7 +162,7 @@ appetitive_summed_hist_lvl7= sum_cluster_hit_hist(appetitive_hit_hist_lvl7)[0]
 
 # how many neurons are shared between activation patterns?
 
-threshold = 50
+threshold = n_init/2
 hops = 4 # how many hops are considered for threshold
 
 indices_over_thres_appetitive = []
@@ -227,7 +230,7 @@ fig, axs = plt.subplots(
     1, 3, figsize=(2, 2)
 )
 
-vmax = 100
+vmax = n_init
 
 ax = axs[0]
 sns.heatmap(appetitive_summed_hist_lvl7.loc[order, 0:4], vmax = vmax, ax = ax, cbar = False)
@@ -237,18 +240,19 @@ ax.set_xlabel('Hops')
 ax.set_title('Appetitive')
 
 ax = axs[1]
+sns.heatmap(aversive_summed_hist_lvl7.loc[order, 0:4], vmax = vmax, ax = ax, cbar = False)
+ax.set_yticks([])
+ax.set_ylabel('')
+ax.set_xlabel('Hops')
+ax.set_title('Aversive')
+
+ax = axs[2]
 sns.heatmap(pd.concat([appetitive_clusters.loc[order], 
                                     intersect_clusters.loc[order], 
                                     aversive_clusters.loc[order]], axis = 1), ax = ax, cbar = False, cmap = cmr.lavender)
 ax.set_yticks([])
 
-ax = axs[2]
-sns.heatmap(aversive_summed_hist_lvl7.loc[order, 0:4], vmax = vmax, ax = ax, cbar = False)
-ax.set_yticks([])
-ax.set_ylabel('')
-ax.set_xlim(5, 0)
-ax.set_xlabel('Hops')
-ax.set_title('Aversive')
+
 
 plt.savefig('cascades/aversive_appetitive/plots/aversive_appetitive_cascades.pdf', format='pdf', bbox_inches='tight')
 
@@ -277,7 +281,7 @@ LHN_skids = pymaid.get_skids_by_annotation('mw LHN')
 MBIN_skids = pymaid.get_skids_by_annotation('mw MBIN')
 KC_skids = pymaid.get_skids_by_annotation('mw KC')
 MBON_skids = pymaid.get_skids_by_annotation('mw MBON')
-FBN_skids = pymaid.get_skids_by_annotation(['mw FBN', 'mw FB2N', 'mw FAN'], allow_partial=True)
+FBN_skids = pymaid.get_skids_by_annotation(['mw FBN', 'mw FB2N', 'mw FAN'])
 CN_skids = pymaid.get_skids_by_annotation('mw CN')
 dVNC_skids = pymaid.get_skids_by_annotation('mw dVNC')
 dSEZ_skids = pymaid.get_skids_by_annotation('mw dSEZ')
@@ -324,5 +328,113 @@ fig, axs = plt.subplots(
 sns.heatmap(pd.concat([appetitive_type, intersection_type, aversive_type], axis = 1), annot=True, fmt=".0%", linewidth = 0.25, vmax = vmax, ax = axs, cmap = cmap, cbar = cbar, annot_kws={"size": fontsize})
 plt.savefig('cascades/aversive_appetitive/plots/celltypes.pdf', bbox_inches='tight')
 
+# %%
+# which types of neurons are in FBN category?
 
+pd.DataFrame(app_skids, columns = ['app_skids']).to_csv('cascades/aversive_appetitive/plots/skids_app_old.csv')
+pd.DataFrame(av_skids, columns = ['avers_skids']).to_csv('cascades/aversive_appetitive/plots/skids_av_old.csv')
+pd.DataFrame(inter_skids, columns = ['intersection_skids']).to_csv('cascades/aversive_appetitive/plots/skids_app_av_intersection_old.csv')
+
+# %%
+# identify pairs of neurons over threshold in app, avs, and inter categories
+# not sure that it's working currently
+import connectome_tools.process_matrix as promat
+
+def skid_to_index(skid, mg):
+    index_match = np.where(mg.meta.index == skid)[0]
+    if(len(index_match)==1):
+        return(int(index_match[0]))
+    if(len(index_match)!=1):
+        print('Not one match for skid %i!' %skid)
+
+threshold = n_init/2
+hops = 4
+
+# import pairs
+pairs = pd.read_csv('data/pairs-2020-05-08.csv', header = 0)
+pairs = pairs.drop(1165)
+
+pairs['left_index'] = [skid_to_index(x, mg) for x in pairs.leftid]
+pairs['right_index'] = [skid_to_index(x, mg) for x in pairs.rightid]
+
+pairs['left_visits_app'] = [sum(appetitive_hit_hist[x, 1:(hops+1)]) for x in pairs.left_index]
+pairs['right_visits_app'] = [sum(appetitive_hit_hist[x, 1:(hops+1)]) for x in pairs.right_index]
+pairs['average_visits_app'] = [(pairs.loc[x].left_visits_app + pairs.loc[x].right_visits_app)/2 for x in pairs.index]
+
+pairs['left_visits_av'] = [sum(aversive_hit_hist[x, 1:(hops+1)]) for x in pairs.left_index]
+pairs['right_visits_av'] = [sum(aversive_hit_hist[x, 1:(hops+1)]) for x in pairs.right_index]
+pairs['average_visits_av'] = [(pairs.loc[x].left_visits_av + pairs.loc[x].right_visits_av)/2 for x in pairs.index]
+
+intersect_pairs = pairs[(pairs.average_visits_app > threshold) & (pairs.average_visits_av > threshold)]
+app_pairs = pairs[(pairs.average_visits_app > threshold) & (pairs.average_visits_av < threshold)]
+av_pairs = pairs[(pairs.average_visits_app < threshold) & (pairs.average_visits_av > threshold)]
+
+app_pairs.to_csv('cascades/aversive_appetitive/plots/skids_app.csv')
+av_pairs.to_csv('cascades/aversive_appetitive/plots/skids_av.csv')
+intersect_pairs.to_csv('cascades/aversive_appetitive/plots/skids_app_av_intersection.csv')
+
+'''
+def skid_to_index(skid, mg):
+    index_match = np.where(mg.meta.index == skid)[0]
+    if(len(index_match)==1):
+        return(index_match[0])
+    if(len(index_match)!=1):
+        print('Not one match for skid %i!' %skid)
+
+threshold = n_init/2
+hops = 4
+
+def pair_visits_threshold(skids, hit_hist, pairs, threshold, hops):
+    over_thres = []
+    for skid in skids:
+        if(skid in pairs.leftid.values):
+            pair_index = np.where(skid == pairs.leftid.values)[0][0]
+
+            left = skid_to_index(skid, mg)
+            right = skid_to_index(pairs.iloc[pair_index, 1], mg)
+
+            left_visits = sum(hit_hist[left, 1:(hops+1)])
+            right_visits = sum(hit_hist[right, 1:(hops+1)])
+
+            average_visits = (left_visits + right_visits)/2
+            if(average_visits>threshold):
+                over_thres.append([True, skid, pairs.iloc[pair_index, :].rightid, left, right, left_visits, right_visits, average_visits])
+            
+        if((skid not in pairs.leftid.values) & (skid in pairs.rightid.values)):
+            pair_index = np.where(skid == pairs.rightid.values)[0][0]
+
+            right = skid_to_index(skid, mg)
+            left = skid_to_index(pairs.iloc[pair_index, 0], mg)
+
+            left_visits = sum(hit_hist[left, 1:(hops+1)])
+            right_visits = sum(hit_hist[right, 1:(hops+1)])
+
+            average_visits = (left_visits + right_visits)/2
+            if(average_visits>threshold):
+                over_thres.append([True, skid, pairs.iloc[pair_index, :].rightid, left, right, left_visits, right_visits, average_visits])
+
+        if((skid not in pairs.leftid.values) & (skid not in pairs.rightid.values)):
+
+            over_thres.append([False, skid, 'nan', skid_to_index(skid, mg), 'nan', sum(appetitive_hit_hist[skid_to_index(skid, mg), 1:4]), 'nan', sum(appetitive_hit_hist[skid_to_index(skid, mg), 1:4])])
+            
+    over_thres = pd.DataFrame(over_thres, columns = ['Pair', 'left_skid', 'right_skid', 
+                                                'left_index', 'right_index', 'left_visits', 'right_visits', 'average_visits'])
+
+    return(over_thres)
+
+app_pairs = pair_visits_threshold(mg.meta.index, appetitive_hit_hist, pairs, threshold, hops)
+avers_pairs = pair_visits_threshold(mg.meta.index, aversive_hit_hist, pairs, threshold, hops)
+
+app_index = app_pairs.average_visits>500
+app_skids_pairs = np.concatenate([app_pairs[app_index].left_skid, 
+                                app_pairs[app_index].right_skid[app_pairs[app_index].right_skid!='nan']]) # remove nans from .right_skids from unpaired neurons
+
+avers_index = avers_pairs.average_visits>500
+avers_skids_pairs = np.concatenate([avers_pairs[avers_index].left_skid, 
+                                app_pairs[avers_index].right_skid[app_pairs[avers_index].right_skid!='nan']]) # remove nans from .right_skids from unpaired neurons
+
+inter_skids_pairs = np.intersect1d(app_skids_pairs, avers_skids_pairs)
+app_skids_pairs = np.setdiff1d(app_skids_pairs, inter_skids_pairs)
+avers_skids_pairs = np.setdiff1d(avers_skids_pairs, inter_skids_pairs)
+'''
 # %%
