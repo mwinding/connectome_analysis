0a2d905dfff4cec2ffda8b76f35ea166f20b14de
mwinding <mwinding@alumni.nd.edu>
2021-01-21 21:16:29 +0000

    identified bilateral neurons and updated plots

--- interhemisphere/ipsi_contra_analysis.py
+++ interhemisphere/ipsi_contra_analysis.py
@@ -46,9 +46,6 @@ inputs = pd.read_csv('VNC_interaction/data/brA1_input_counts.csv', index_col = 0
 inputs = pd.DataFrame(inputs.values, index = inputs.index, columns = ['axon_input', 'dendrite_input'])
 pairs = pd.read_csv('VNC_interaction/data/pairs-2020-10-26.csv', header = 0) # import pairs
 
-# load projectome 
-projectome = pd.read_csv('data/projectome.csv')
-
 # load cluster data
 clusters = pd.read_csv('cascades/data/meta-method=color_iso-d=8-bic_ratio=0.95-min_split=32.csv', index_col = 0, header = 0)
 
@@ -71,11 +68,199 @@ order_df = order_df.sort_values(by = 'node_visit_order')
 order = list(order_df.cluster)
 
 # %%
-# ipsi/contra per cluster
+# contra/bilateral character plot
 
-# these annotations includes brain neurons, sensories, and some others
-ipsi = pymaid.get_skids_by_annotation('mw ipsilateral')
-contra = pymaid.get_skids_by_annotation('mw contralateral')
+left = pymaid.get_skids_by_annotation('mw left')
+right = pymaid.get_skids_by_annotation('mw right')
+
+projectome = pd.read_csv('interhemisphere/data/projectome_mw_brain_matrix_A1_split.csv', index_col = 0, header = 0)
+
+is_left = []
+for skid in projectome.skeleton:
+    if(skid in left):
+        is_left.append([skid, 1])
+    if(skid in right):
+        is_left.append([skid, 0])
+    if((skid not in right) & (skid not in left)):
+        is_left.append([skid, -1])
+is_left = pd.DataFrame(is_left, columns = ['skid', 'is_left'])
+
+projectome['is_left']=is_left.is_left.values
+proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['Brain Hemisphere left', 'Brain Hemisphere right'].sum()
+
+right_contra_axon_outputs = proj_group.loc[(br, 0, 1, 0), :] # right side, axon outputs
+left_contra_axon_outputs = proj_group.loc[(br, 1, 1, 0), :] # left side, axon outputs
+
+right_contra_axon_outputs['ratio'] = right_contra_axon_outputs['Brain Hemisphere left']/(right_contra_axon_outputs['Brain Hemisphere left'] + right_contra_axon_outputs['Brain Hemisphere right'])
+left_contra_axon_outputs['ratio'] = left_contra_axon_outputs['Brain Hemisphere right']/(left_contra_axon_outputs['Brain Hemisphere left'] + left_contra_axon_outputs['Brain Hemisphere right'])
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+sns.distplot(right_contra_axon_outputs.ratio, ax=ax, kde=False, bins=20)
+sns.distplot(left_contra_axon_outputs.ratio, ax=ax, kde=False, bins=20, color='gray')
+ax.set(yticks=[], xticks=[])
+fig.savefig('interhemisphere/plots/Br_axon-output_ipsi-vs-contralateral.pdf', format='pdf', bbox_inches='tight')
+
+right_den_inputs = proj_group.loc[(br, 0, 0, 1), :] # right side, dendrite inputs
+left_den_inputs = proj_group.loc[(br, 1, 0, 1), :] # left side, dendrite inputs
+
+right_den_inputs['ratio'] = right_den_inputs['Brain Hemisphere left']/(right_den_inputs['Brain Hemisphere left'] + right_den_inputs['Brain Hemisphere right'])
+left_den_inputs['ratio'] = left_den_inputs['Brain Hemisphere right']/(left_den_inputs['Brain Hemisphere left'] + left_den_inputs['Brain Hemisphere right'])
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+sns.distplot(right_den_inputs.ratio, ax=ax, kde=False, bins=20)
+sns.distplot(left_den_inputs.ratio, ax=ax, kde=False, bins=20, color='gray')
+ax.set(yticks=[], xticks=[])
+fig.savefig('interhemisphere/plots/Br_dendrite-input_ipsi-vs-contralateral.pdf', format='pdf', bbox_inches='tight')
+
+# same analysis with A1 for comparison
+meshes_left = ['Brain Hemisphere left', 'SEZ_left', 'T1_left', 'T2_left', 'T3_left', 'A1_left', 'A2_left', 'A3_left', 'A4_left', 'A5_left', 'A6_left', 'A7_left', 'A8_left']
+meshes_right = ['Brain Hemisphere right', 'SEZ_right', 'T1_right', 'T2_right', 'T3_right', 'A1_right', 'A2_right', 'A3_right', 'A4_right', 'A5_right', 'A6_right', 'A7_right', 'A8_right']
+meshes = meshes_left + meshes_right
+
+projectome['summed_left'] = projectome[meshes_left].sum(axis=1)
+projectome['summed_right'] = projectome[meshes_right].sum(axis=1)
+proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['summed_left', 'summed_right'].sum()
+
+A1_local = list(np.setdiff1d(A1, A1_ascending))
+right_contra_axon_outputs = proj_group.loc[(A1, 0, 1, 0), :] # right side, axon outputs
+left_contra_axon_outputs = proj_group.loc[(A1, 1, 1, 0), :] # left side, axon outputs
+
+right_contra_axon_outputs['ratio'] = right_contra_axon_outputs['summed_left']/(right_contra_axon_outputs['summed_left'] + right_contra_axon_outputs['summed_right'])
+left_contra_axon_outputs['ratio'] = left_contra_axon_outputs['summed_right']/(left_contra_axon_outputs['summed_left'] + left_contra_axon_outputs['summed_right'])
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+sns.distplot(right_contra_axon_outputs.ratio, ax=ax, kde=False, bins=20)
+sns.distplot(left_contra_axon_outputs.ratio, ax=ax, kde=False, bins=20, color='gray')
+ax.set(yticks=[], xticks=[])
+fig.savefig('interhemisphere/plots/A1_axon-output_ipsi-vs-contralateral.pdf', format='pdf', bbox_inches='tight')
+
+right_den_inputs = proj_group.loc[(A1, 0, 0, 1), :] # right side, dendrite inputs
+left_den_inputs = proj_group.loc[(A1, 1, 0, 1), :] # left side, dendrite inputs
+
+right_den_inputs['ratio'] = right_den_inputs['summed_left']/(right_den_inputs['summed_left'] + right_den_inputs['summed_right'])
+left_den_inputs['ratio'] = left_den_inputs['summed_right']/(left_den_inputs['summed_left'] + left_den_inputs['summed_right'])
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+sns.distplot(right_den_inputs.ratio, ax=ax, kde=False, bins=20)
+sns.distplot(left_den_inputs.ratio, ax=ax, kde=False, bins=20, color='gray')
+ax.set(yticks=[], xticks=[])
+fig.savefig('interhemisphere/plots/A1_dendrite-input_ipsi-vs-contralateral.pdf', format='pdf', bbox_inches='tight')
+# %%
+# pair-wise contra/bilateral character
+# use this to proofread pairs and define all neurons as ipsi-, bi-, or contralateral
+from connectome_tools.process_matrix import Adjacency_matrix, Promat
+
+meshes_left = ['Brain Hemisphere left', 'SEZ_left', 'T1_left', 'T2_left', 'T3_left', 'A1_left', 'A2_left', 'A3_left', 'A4_left', 'A5_left', 'A6_left', 'A7_left', 'A8_left']
+meshes_right = ['Brain Hemisphere right', 'SEZ_right', 'T1_right', 'T2_right', 'T3_right', 'A1_right', 'A2_right', 'A3_right', 'A4_right', 'A5_right', 'A6_right', 'A7_right', 'A8_right']
+meshes = meshes_left + meshes_right
+
+projectome['summed_left'] = projectome[meshes_left].sum(axis=1)
+projectome['summed_right'] = projectome[meshes_right].sum(axis=1)
+proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['summed_left', 'summed_right'].sum()
+#proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['Brain Hemisphere left', 'Brain Hemisphere right'].sum()
+br_pairs = Promat.extract_pairs_from_list(br, pairs)[0]
+
+right_contra_axon_outputs = proj_group.loc[(br, 0, 1, 0), :] # right side, axon outputs
+left_contra_axon_outputs = proj_group.loc[(br, 1, 1, 0), :] # left side, axon outputs
+
+right_contra_axon_outputs['ratio'] = right_contra_axon_outputs['summed_left']/(right_contra_axon_outputs['summed_left'] + right_contra_axon_outputs['summed_right'])
+left_contra_axon_outputs['ratio'] = left_contra_axon_outputs['summed_right']/(left_contra_axon_outputs['summed_left'] + left_contra_axon_outputs['summed_right'])
+
+br_pairs = Promat.extract_pairs_from_list(br, pairs)[0]
+
+contra_pairs = []
+for i in range(len(br_pairs)):
+    if((br_pairs.leftid[i] in [x[0] for x in left_contra_axon_outputs.index]) & (br_pairs.rightid[i] in [x[0] for x in right_contra_axon_outputs.index])):
+        ratio_l = left_contra_axon_outputs.loc[br_pairs.leftid[i], 'ratio'].values[0]
+        ratio_r = right_contra_axon_outputs.loc[br_pairs.rightid[i], 'ratio'].values[0]
+        contra_pairs.append([br_pairs.leftid[i], br_pairs.rightid[i], ratio_l, ratio_r, (ratio_l+ratio_r)/2])
+
+contra_pairs = pd.DataFrame(contra_pairs, columns = ['leftid', 'rightid', 'ratio_left', 'ratio_right', 'ratio_aver'])
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+sns.distplot(contra_pairs.ratio_aver, ax=ax, kde=False, bins=20)
+ax.set(yticks=[], xticks=[])
+fig.savefig('interhemisphere/plots/pairs_Br_axon-output_ipsi-vs-contralateral.pdf', format='pdf', bbox_inches='tight')
+'''
+# check neurons on the edge of 1 and 0
+almost_contra = (contra_pairs.ratio_aver>0.8) & (contra_pairs.ratio_aver<1.0)
+almost_contra_skids = list(contra_pairs[almost_contra].leftid.values) + list(contra_pairs[almost_contra].rightid.values)
+pymaid.add_annotations(almost_contra_skids, 'mw check contra')
+
+almost_ipsi = (contra_pairs.ratio_aver>0.0) & (contra_pairs.ratio_aver<0.35)
+almost_ipsi = (contra_pairs.ratio_aver==0.0)
+almost_ipsi_skids = np.intersect1d(list(contra_pairs[almost_ipsi].leftid.values) + list(contra_pairs[almost_ipsi].rightid.values), contra)
+pymaid.add_annotations(almost_ipsi_skids, 'mw check ipsi')
+'''
+# %%
+# annotate ipsilateral, bilateral, and contralateral axons
+# remove neurons with "mw brain few synapses"
+'''
+ipsi_annotated = pymaid.get_skids_by_annotation('mw ipsilateral axon')
+bilateral_annotated = pymaid.get_skids_by_annotation('mw bilateral axon')
+contra_annotated = pymaid.get_skids_by_annotation('mw contralateral axon')
+few_synapses = pymaid.get_skids_by_annotation('mw brain few synapses')
+
+# exclude skids that were manually annotated or are only partially differentiated
+excluded = ipsi_annotated + bilateral_annotated + contra_annotated + few_synapses
+
+ipsi_skids = list(contra_pairs[contra_pairs.ratio_aver==0].leftid) + list(contra_pairs[contra_pairs.ratio_aver==0].rightid)
+contra_skids = list(contra_pairs[contra_pairs.ratio_aver==1].leftid) + list(contra_pairs[contra_pairs.ratio_aver==1].rightid)
+bilateral_skids = list(contra_pairs[(contra_pairs.ratio_aver<1) & (contra_pairs.ratio_aver>0)].leftid) + list(contra_pairs[(contra_pairs.ratio_aver<1) & (contra_pairs.ratio_aver>0)].rightid)
+
+ipsi_skids = list(np.setdiff1d(ipsi_skids, excluded))
+contra_skids = list(np.setdiff1d(contra_skids, excluded))
+bilateral_skids = list(np.setdiff1d(bilateral_skids, excluded))
+
+ipsi_skids_pair = Promat.extract_pairs_from_list(ipsi_skids, pairs)[0]
+contra_skids_pair = Promat.extract_pairs_from_list(contra_skids, pairs)[0]
+bilateral_skids_pair = Promat.extract_pairs_from_list(bilateral_skids, pairs)[0]
+
+contra_pairs.index = contra_pairs.leftid
+sns.distplot(contra_pairs.loc[ipsi_skids_pair.leftid].ratio_aver, kde=False, bins=20)
+sns.distplot(contra_pairs.loc[bilateral_skids_pair.leftid].ratio_aver, kde=False, bins=20)
+sns.distplot(contra_pairs.loc[contra_skids_pair.leftid].ratio_aver, kde=False, bins=20)
+
+pymaid.add_annotations(ipsi_skids, 'mw ipsilateral axon')
+pymaid.add_annotations(contra_skids, 'mw contralateral axon')
+pymaid.add_annotations(bilateral_skids, 'mw bilateral axon')
+'''
+# %%
+# plot ipsi, bi, contra
+ipsi_annotated = pymaid.get_skids_by_annotation('mw ipsilateral axon')
+bilateral_annotated = pymaid.get_skids_by_annotation('mw bilateral axon')
+contra_annotated = pymaid.get_skids_by_annotation('mw contralateral axon')
+
+ipsi_skids_pair = Promat.extract_pairs_from_list(ipsi_annotated, pairs)[0]
+contra_skids_pair = Promat.extract_pairs_from_list(contra_annotated, pairs)[0]
+bilateral_skids_pair = Promat.extract_pairs_from_list(bilateral_annotated, pairs)[0]
+
+contra_pairs.index = contra_pairs.leftid
+
+# set up bins
+max_val = 1
+step = max_val/10
+max_val = max_val + step + step/2
+bins = [x for x in np.arange(0, max_val, step-step/2)]
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+sns.distplot(contra_pairs.loc[ipsi_skids_pair.leftid.values].ratio_aver, kde=False, bins=bins, ax=ax)
+sns.distplot(contra_pairs.loc[bilateral_skids_pair.leftid.values].ratio_aver, kde=False, bins=bins, ax=ax)
+sns.distplot(contra_pairs.loc[contra_skids_pair.leftid.values].ratio_aver, kde=False, bins=bins, ax=ax)
+ax.set(yticks=[])
+
+fig, ax = plt.subplots(1,1, figsize=(1,2))
+ax.hist(contra_pairs.loc[ipsi_skids_pair.leftid.values].ratio_aver, bins=bins)
+ax.hist(contra_pairs.loc[bilateral_skids_pair.leftid.values].ratio_aver, bins=bins)
+ax.hist(contra_pairs.loc[contra_skids_pair.leftid.values].ratio_aver, bins=bins)
+fig.savefig('interhemisphere/plots/pairs_Br_axon-output_ipsi-vs-contralateral_colored.pdf', format='pdf', bbox_inches='tight')
+
+# %%
+# ipsi/contra/bilateral per cluster
+
+ipsi = pymaid.get_skids_by_annotation('mw ipsilateral axon')
+contra = pymaid.get_skids_by_annotation('mw contralateral axon')
+bi = pymaid.get_skids_by_annotation('mw bilateral axon')
 
 # integration types per cluster
 cluster_lvl7 = [[key, lvl7.groups[key].values] for key in lvl7.groups.keys()]
@@ -84,23 +269,33 @@ cluster_lvl7.set_index('key', inplace=True)
 
 ipsi_contra_clusters = []
 for key in cluster_lvl7.index:
-    ipsi_sum = len(np.intersect1d(cluster_lvl7.loc[key].skids, ipsi))/len(cluster_lvl7.loc[key].skids)
-    contra_sum = len(np.intersect1d(cluster_lvl7.loc[key].skids, contra))/len(cluster_lvl7.loc[key].skids)
-    ipsi_frac = ipsi_sum/(ipsi_sum+contra_sum)
-    contra_frac = contra_sum/(ipsi_sum+contra_sum)
-    ipsi_contra_clusters.append([key, ipsi_frac, contra_frac])
+    ipsi_sum = len(np.intersect1d(cluster_lvl7.loc[key].skids, ipsi))#/len(cluster_lvl7.loc[key].skids)
+    contra_sum = len(np.intersect1d(cluster_lvl7.loc[key].skids, contra))#/len(cluster_lvl7.loc[key].skids)
+    bi_sum = len(np.intersect1d(cluster_lvl7.loc[key].skids, bi))#/len(cluster_lvl7.loc[key].skids)
 
-ipsi_contra_clusters = pd.DataFrame(ipsi_contra_clusters, columns = ['key', 'ipsi', 'contra'])
+    if((ipsi_sum + contra_sum + bi_sum)>0):
+        ipsi_frac = ipsi_sum/(ipsi_sum + contra_sum + bi_sum)
+        contra_frac = contra_sum/(ipsi_sum + contra_sum + bi_sum)
+        bi_frac = bi_sum/(ipsi_sum + contra_sum + bi_sum)
+        ipsi_contra_clusters.append([key, ipsi_frac, bi_frac, contra_frac])
+    else:
+        ipsi_contra_clusters.append([key, 0, 0, 0])
+
+ipsi_contra_clusters = pd.DataFrame(ipsi_contra_clusters, columns = ['key', 'ipsi', 'bi', 'contra'])
 ipsi_contra_clusters.set_index('key', inplace=True)
 ipsi_contra_clusters = ipsi_contra_clusters.loc[order, :]
 
 ind = [x for x in range(0, len(cluster_lvl7))]
 fig, ax = plt.subplots(1,1, figsize=(3,2))
 plt.bar(ind, ipsi_contra_clusters.ipsi.values)
-plt.bar(ind, ipsi_contra_clusters.contra.values, bottom = ipsi_contra_clusters.ipsi, color='violet')
+plt.bar(ind, ipsi_contra_clusters.bi.values, bottom = ipsi_contra_clusters.ipsi)
+plt.bar(ind, ipsi_contra_clusters.contra.values, bottom = ipsi_contra_clusters.ipsi + ipsi_contra_clusters.bi)
 fig.savefig('interhemisphere/plots/ipsi_contra_makeup_clusters.pdf', format='pdf', bbox_inches='tight')
 # %%
 # amount of ipsi/contra per cell type
+ipsi = pymaid.get_skids_by_annotation('mw ipsilateral axon')
+contra = pymaid.get_skids_by_annotation('mw contralateral axon')
+bi = pymaid.get_skids_by_annotation('mw bilateral axon')
 
 # set cell types
 A1_ascending = pymaid.get_skids_by_annotation('mw A1 neurons paired ascending')
@@ -140,7 +335,9 @@ CN = list(np.setdiff1d(CN, LHN + FBN_all)) # 'CN' means exclusive CNs that are n
 dSEZ = list(np.setdiff1d(dSEZ, MBON + MBIN + LHN + CN + KC + dVNC + PN + FBN_all))
 pre_dVNC = list(np.setdiff1d(pre_dVNC, MBON + MBIN + LHN + CN + KC + dSEZ + dVNC + PN + FBN_all + asc_all + sens_all + A00c)) # 'pre_dVNC' must have no other category assignment
 
-celltypes = [list(np.setdiff1d(br, RGN)), sens_all, PN, LHN, MBIN, KC, MBON, FBN_all, CN, pre_dVNC, dSEZ, dVNC]
+few_synapses = pymaid.get_skids_by_annotation('mw brain few synapses')
+
+celltypes = [list(np.setdiff1d(br, RGN+few_synapses)), sens_all, PN, LHN, MBIN, list(np.setdiff1d(KC, few_synapses)), MBON, FBN_all, CN, pre_dVNC, dSEZ, dVNC]
 celltype_names = ['Total', 'Sens', 'PN', 'LHN', 'MBIN', 'KC', 'MBON', 'MB-FBN', 'CN', 'pre-dVNC', 'dSEZ', 'dVNC']
 
 ipsi_contra_celltypes = []
@@ -148,49 +345,26 @@ unknown_list = []
 for i, celltype in enumerate(celltypes):
     ipsi_sum = len(np.intersect1d(celltype, ipsi))
     contra_sum = len(np.intersect1d(celltype, contra))
+    bi_sum = len(np.intersect1d(celltype, bi))
+
     ipsi_frac = ipsi_sum/len(celltype)
     contra_frac = contra_sum/len(celltype)
+    bi_frac = bi_sum/len(celltype)
 
-    ipsi_contra_celltypes.append([celltype_names[i], ipsi_frac, contra_frac])
+    ipsi_contra_celltypes.append([celltype_names[i], ipsi_frac, bi_frac, contra_frac])
 
-    unknown = np.setdiff1d(celltype, (ipsi+contra))
+    unknown = np.setdiff1d(celltype, (ipsi+contra+bi))
     unknown_list.append(unknown)
 
-ipsi_contra_celltypes = pd.DataFrame(ipsi_contra_celltypes, columns = ['celltype', 'ipsi', 'contra'])
+ipsi_contra_celltypes = pd.DataFrame(ipsi_contra_celltypes, columns = ['celltype', 'ipsi', 'bi', 'contra'])
 
 ind = [x for x in range(0, len(ipsi_contra_celltypes))]
 fig, ax = plt.subplots(1,1, figsize=(2,2))
 plt.bar(ind, ipsi_contra_celltypes.ipsi.values)
-plt.bar(ind, ipsi_contra_celltypes.contra.values, bottom = ipsi_contra_celltypes.ipsi, color='violet')
+plt.bar(ind, ipsi_contra_celltypes.bi.values, bottom = ipsi_contra_celltypes.ipsi)
+plt.bar(ind, ipsi_contra_celltypes.contra.values, bottom = ipsi_contra_celltypes.ipsi+ipsi_contra_celltypes.bi)
 plt.xticks(rotation=45, ha='right')
 ax.set(xticklabels = celltype_names, xticks=np.arange(0, len(ipsi_contra_celltypes), 1))
 fig.savefig('interhemisphere/plots/ipsi_contra_makeup_celltypes.pdf', format='pdf', bbox_inches='tight')
 
 # %%
-# contra/bilateral character plot
-
-left = pymaid.get_skids_by_annotation('mw left')
-right = pymaid.get_skids_by_annotation('mw right')
-
-projectome = pd.read_csv('interhemisphere/data/projectome_split.csv', index_col = 0, header = 0)
-
-is_left = []
-for skid in projectome.skeleton:
-    if(skid in left):
-        is_left.append([skid, 1])
-    if(skid in right):
-        is_left.append([skid, 0])
-    if((skid not in right) & (skid not in left)):
-        is_left.append([skid, -1])
-is_left = pd.DataFrame(is_left, columns = ['skid', 'is_left'])
-
-projectome['is_left']=is_left.is_left.values
-proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['Brain Hemisphere left', 'Brain Hemisphere right'].sum()
-
-right_contra_axon_outputs = proj_group.loc[(contra, 0, 1, 0), :] # right side, axon outputs
-left_contra_axon_outputs = proj_group.loc[(contra, 1, 1, 0), :] # left side, axon outputs
-
-right_den_inputs = proj_group.loc[(slice(None), 0, 0, 1), :] # right side, dendrite inputs
-left_den_inputs = proj_group.loc[(slice(None), 1, 0, 1), :] # left side, dendrite inputs
-
-right_contra_axon_outputs['ratio'] = right_contra_axon_outputs['Brain Hemisphere right']/right_contra_axon_outputs['Brain Hemisphere left']
