24bc02c5dad30afec4b2b8d8e596a36c0d5a86d9
mwinding <mwinding@alumni.nd.edu>
2021-01-25 17:09:26 +0000

    investigated asymmetrical bilateral neurons

--- interhemisphere/bilateral_neuron_analysis.py
+++ interhemisphere/bilateral_neuron_analysis.py
@@ -137,9 +137,9 @@ def bilateral_ad_connections(leftid, rightid, skeletons, inputs, normalize=True)
     return(data)
 
 # set threshold to -1 to not use a threshold; like this by default
-def ipsi_contra_ds_partners(bilateral_leftid, bilateral_rightid, skeletons, inputs, threshold=-1):
+def ipsi_contra_ds_partners(bilateral_leftid, bilateral_rightid, skeletons, inputs, threshold=-1, normalize=True):
     # look for ipsi and contra downstream partners over threshold
-    bi_ad_connect = bilateral_ad_connections(bilateral_leftid, bilateral_rightid, skeletons, inputs)
+    bi_ad_connect = bilateral_ad_connections(bilateral_leftid, bilateral_rightid, skeletons, inputs, normalize=normalize)
     bi_ad_connect_index1 = bi_ad_connect.set_index(['connection_type', 'source'])
     if(len(bi_ad_connect_index1)==0):
         return([])
@@ -158,7 +158,7 @@ def ipsi_contra_ds_partners(bilateral_leftid, bilateral_rightid, skeletons, inpu
     if(type(contra_right_partners)==pd.Series):
         contra_right_partners = list(contra_right_partners)
 
-    contra_pair_partners = Promat.extract_pairs_from_list(contra_left_partners + contra_right_partners, pairs)[0]
+    contra_pair_partners = Promat.extract_pairs_from_list(np.unique(contra_left_partners + contra_right_partners), pairs)[0] # added np.unique to deal with issues with the few bilateral dendrites (MBONs)
 
     # identify and average contralateral inputs
     bi_ad_connect_index2 = bi_ad_connect.set_index(['connection_type', 'source', 'target'])
@@ -190,7 +190,7 @@ def ipsi_contra_ds_partners(bilateral_leftid, bilateral_rightid, skeletons, inpu
     if(type(ipsi_right_partners)==pd.Series):
         ipsi_right_partners = list(ipsi_right_partners)
 
-    ipsi_pair_partners = Promat.extract_pairs_from_list(ipsi_left_partners + ipsi_right_partners, pairs)[0]
+    ipsi_pair_partners = Promat.extract_pairs_from_list(np.unique(ipsi_left_partners + ipsi_right_partners), pairs)[0] # added np.unique to deal with issues with the few bilateral dendrites (MBONs)
 
     # identify and average ipsilateral inputs
     bi_ad_connect_index2 = bi_ad_connect.set_index(['connection_type', 'source', 'target'])
@@ -218,10 +218,10 @@ issues_list = []
 data_list = []
 for i in tqdm(range(1, len(bilateral_pairs))):
     try:
-        data = ipsi_contra_ds_partners(bilateral_pairs.leftid[i], bilateral_pairs.rightid[i], skeletons, inputs)
+        data = ipsi_contra_ds_partners(bilateral_pairs.leftid[i], bilateral_pairs.rightid[i], skeletons, inputs, normalize=True)
         data_list.append(data)
     except:
-        print(f'problem with {i}')
+        print(f'problem with index {i}, skids {bilateral_pairs.leftid[i]}, {bilateral_pairs.rightid[i]}')
         issues_list.append(i)
 
 filtered_data = [x for x in data_list if type(x)==pd.DataFrame]
@@ -234,10 +234,280 @@ data = data.set_index(['source_pairid', 'connection_type'])
 merged_list = []
 for i in range(len(pair_ids)):
     if(('contralateral' in data.loc[pair_ids[i]].index) & ('ipsilateral' in data.loc[pair_ids[i]].index)): # super hacky, need to check the data
-        ipsi_data = pd.DataFrame([data.loc[(pair_ids[i], 'ipsilateral')].average_input.values], index = [f'{pair_ids[i]}-ipsi'], columns=list(data.loc[(pair_ids[i], 'ipsilateral')].leftid.values))
+        ipsi_data = pd.DataFrame([data.loc[(pair_ids[i], 'ipsilateral')].average_input.values], index = [pair_ids[i]], columns=list(data.loc[(pair_ids[i], 'ipsilateral')].leftid.values))
         contra_data = pd.DataFrame([data.loc[(pair_ids[i], 'contralateral')].average_input.values], index = [f'{pair_ids[i]}-contra'], columns=list(data.loc[(pair_ids[i], 'contralateral')].leftid.values))
 
         merged_data = pd.concat([ipsi_data, contra_data], axis=0)
         merged_data.fillna(0, inplace=True)
         merged_list.append(merged_data)
+# %%
+# calculate cosine similarity between ipsi and contra connections in bilateral pairs
+
+def cosine_similarity(a, b):
+    dot = np.dot(a, b)
+    norma = np.linalg.norm(a)
+    normb = np.linalg.norm(b)
+    cos = dot / (norma * normb)
+
+    return(cos)
+
+cos_list = []
+for pair in merged_list:
+    cos = cosine_similarity(list(pair.iloc[0,:]), list(pair.iloc[1,:]))
+    cos_list.append([pair.index[0], cos])
+
+cos_list = pd.DataFrame(cos_list, columns = ['pairid', 'similarity_ipsi_contra'])
+# %%
+# combine with "ipsi-contra" measure
+# below is copy-paste from ipsi_contra_analysis.py
+
+left = pymaid.get_skids_by_annotation('mw left')
+right = pymaid.get_skids_by_annotation('mw right')
+
+projectome = pd.read_csv('interhemisphere/data/projectome_mw_brain_matrix_A1_split.csv', index_col = 0, header = 0)
+
+is_left = []
+for skid in projectome.skeleton:
+    if(skid in left):
+        is_left.append([skid, 1])
+    if(skid in right):
+        is_left.append([skid, 0])
+    if((skid not in right) & (skid not in left)):
+        is_left.append([skid, -1])
+is_left = pd.DataFrame(is_left, columns = ['skid', 'is_left'])
+
+projectome['is_left']=is_left.is_left.values
+
+from connectome_tools.process_matrix import Adjacency_matrix, Promat
+
+meshes_left = ['Brain Hemisphere left', 'SEZ_left', 'T1_left', 'T2_left', 'T3_left', 'A1_left', 'A2_left', 'A3_left', 'A4_left', 'A5_left', 'A6_left', 'A7_left', 'A8_left']
+meshes_right = ['Brain Hemisphere right', 'SEZ_right', 'T1_right', 'T2_right', 'T3_right', 'A1_right', 'A2_right', 'A3_right', 'A4_right', 'A5_right', 'A6_right', 'A7_right', 'A8_right']
+meshes = meshes_left + meshes_right
+
+projectome['summed_left'] = projectome[meshes_left].sum(axis=1)
+projectome['summed_right'] = projectome[meshes_right].sum(axis=1)
+proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['summed_left', 'summed_right'].sum()
+#proj_group = projectome.groupby(['skeleton', 'is_left','is_axon', 'is_input'])['Brain Hemisphere left', 'Brain Hemisphere right'].sum()
+br_pairs = Promat.extract_pairs_from_list(br, pairs)[0]
+
+right_contra_axon_outputs = proj_group.loc[(br, 0, 1, 0), :] # right side, axon outputs
+left_contra_axon_outputs = proj_group.loc[(br, 1, 1, 0), :] # left side, axon outputs
+
+right_contra_axon_outputs['ratio'] = right_contra_axon_outputs['summed_left']/(right_contra_axon_outputs['summed_left'] + right_contra_axon_outputs['summed_right'])
+left_contra_axon_outputs['ratio'] = left_contra_axon_outputs['summed_right']/(left_contra_axon_outputs['summed_left'] + left_contra_axon_outputs['summed_right'])
+
+br_pairs = Promat.extract_pairs_from_list(br, pairs)[0]
+
+contra_pairs = []
+for i in range(len(br_pairs)):
+    if((br_pairs.leftid[i] in [x[0] for x in left_contra_axon_outputs.index]) & (br_pairs.rightid[i] in [x[0] for x in right_contra_axon_outputs.index])):
+        ratio_l = left_contra_axon_outputs.loc[br_pairs.leftid[i], 'ratio'].values[0]
+        ratio_r = right_contra_axon_outputs.loc[br_pairs.rightid[i], 'ratio'].values[0]
+        contra_pairs.append([br_pairs.leftid[i], br_pairs.rightid[i], ratio_l, ratio_r, (ratio_l+ratio_r)/2])
+
+contra_pairs = pd.DataFrame(contra_pairs, columns = ['leftid', 'rightid', 'ratio_left', 'ratio_right', 'ratio_aver'])
+contra_pairs.set_index('leftid', inplace=True)
+# add ratio data to cosine similarity
+cos_list['ratio'] = abs(contra_pairs.loc[cos_list.pairid, 'ratio_aver'].values-0.5)
+# %%
+# plot cosine similarity
+
+fig, ax = plt.subplots(1,1, figsize=(2,2))
+sns.distplot(cos_list.similarity_ipsi_contra, kde=False, bins=40, rug=True, rug_kws={"linewidth": 0.25, "alpha": 0.75, "height": 0.04}, ax=ax, color=sns.color_palette()[1])
+fig.savefig('interhemisphere/plots/cosine-similarity_ipsi-vs-contra-partners.pdf', format='pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots(1,1, figsize=(.75,1.75))
+sns.swarmplot(cos_list.similarity_ipsi_contra, orient='v', size=2, ax=ax, color=sns.color_palette()[1])
+fig.savefig('interhemisphere/plots/cosine-similarity_ipsi-vs-contra-partners_swarm.pdf', format='pdf', bbox_inches='tight')
+
+sns.jointplot(x=cos_list.similarity_ipsi_contra, y=cos_list.ratio, kind='reg', scatter_kws={'s': 1}, size=5, ylim=(0.5,0), xlim=(0,1), color=sns.color_palette()[1])
+plt.savefig('interhemisphere/plots/cosine-similarity-ipsi-contra-partners_vs_bilateral-character.pdf', format='pdf', bbox_inches='tight')
+
+# %%
+# identify ds neurons from each bilateral neuron: ipsi and contra separately
+
+bilateral_partners = []
+for pair in merged_list:
+    ipsi_partners = list(pair.loc[:, pair.iloc[0, :]>=0.01].columns)
+    contra_partners = list(pair.loc[:, pair.iloc[1, :]>=0.01].columns)
+
+    bilateral_partners.append([pair.index[0], ipsi_partners, contra_partners])
+
+bilateral_partners = pd.DataFrame(bilateral_partners, columns = ['pairid', 'ipsi_ds_partners', 'contra_ds_partners'])
+
+iou_list=[]
+for i in range(len(bilateral_partners)):
+    if((len(bilateral_partners.loc[i].ipsi_ds_partners)>0) & (len(bilateral_partners.loc[i].contra_ds_partners)>0)):
+        iou = len(np.intersect1d(bilateral_partners.loc[i].ipsi_ds_partners, bilateral_partners.loc[i].contra_ds_partners))/((len(bilateral_partners.loc[i].contra_ds_partners)+len(bilateral_partners.loc[i].ipsi_ds_partners))/2)
+        
+    if((len(bilateral_partners.loc[i].ipsi_ds_partners)==0) & (len(bilateral_partners.loc[i].contra_ds_partners)==0)):
+        iou = -1
+    iou_list.append(iou)
+
+sns.distplot(iou_list, bins=40, kde=False)
+
+cos_list['iou'] = iou_list
+cos_list['ipsi_ds_partners'] = bilateral_partners.ipsi_ds_partners
+cos_list['contra_ds_partners'] = bilateral_partners.contra_ds_partners
+cos_list.set_index(cos_list.pairid, inplace=True, drop=True)
+
+asymmetric_ipsi_contra = []
+for i in cos_list.index:
+
+    if((len(cos_list.loc[i].ipsi_ds_partners)==0) | (len(cos_list.loc[i].contra_ds_partners)==0)):
+        continue
+
+    intersect = np.intersect1d(cos_list.loc[i].ipsi_ds_partners, cos_list.loc[i].contra_ds_partners)
+    if((len(intersect)/len(cos_list.loc[i].ipsi_ds_partners)==1) | (len(intersect)/len(cos_list.loc[i].contra_ds_partners)==1)):
+        continue
+
+    bool_list = cos_list[(cos_list.iou<=0.25)] # (cos_list.similarity_ipsi_contra<=0.5) & (cos_list.ratio<=0.25)
+    if(i in bool_list.index):
+        asymmetric_ipsi_contra.append(bool_list.loc[i])
+
+asymmetric_ipsi_contra = pd.DataFrame(asymmetric_ipsi_contra)
+
+# %%
+# plot ipsi_contra neuron networks
+from connectome_tools.process_matrix import Promat
+from connectome_tools.cascade_analysis import Celltype, Celltype_Analyzer
+
+celltypes, celltype_names = Promat.celltypes()
+all_celltypes = list(map(lambda pair: Celltype(pair[0], pair[1]), zip(celltype_names, celltypes)))
+ct_analyzer = Celltype_Analyzer(all_celltypes)
+
+# general bilaterals
+ipsi_partners_all = [x for sublist in list(cos_list.ipsi_ds_partners) for x in sublist]
+contra_partners_all = [x for sublist in list(cos_list.contra_ds_partners) for x in sublist]
+bilateral_analyzer = Celltype_Analyzer([Celltype('ipsi_partners', ipsi_partners_all), Celltype('contra_partners', contra_partners_all)])
+bilateral_analyzer.set_known_types(all_celltypes)
+bilateral_memberships = bilateral_analyzer.memberships()
+
+# asymmetric bilaterals
+ipsi_partners = [x for sublist in list(asymmetric_ipsi_contra.ipsi_ds_partners) for x in sublist]
+contra_partners = [x for sublist in list(asymmetric_ipsi_contra.contra_ds_partners) for x in sublist]
+asym_bilateral_analyzer = Celltype_Analyzer([Celltype('ipsi_partners', ipsi_partners), Celltype('contra_partners', contra_partners)])
+asym_bilateral_analyzer.set_known_types(all_celltypes)
+asym_bilateral_memberships = asym_bilateral_analyzer.memberships()
+
+# individual asymmetric bilateral pathways
+asym_bilateral_paths = []
+for pairid in asymmetric_ipsi_contra.index:
+    ipsi_partners = [x for x in list(asymmetric_ipsi_contra.loc[pairid].ipsi_ds_partners)]
+    contra_partners = [x for x in list(asymmetric_ipsi_contra.loc[pairid].contra_ds_partners)]
+    ipsi_partners = Celltype(f'{pairid}_ipsi_partners', ipsi_partners)
+    contra_partners = Celltype(f'{pairid}_contra_partners', contra_partners)
+    asym_bilateral_paths.append(ipsi_partners)
+    asym_bilateral_paths.append(contra_partners)
+
+paths_asym_bilat_analyzer = Celltype_Analyzer(asym_bilateral_paths)
+paths_asym_bilat_analyzer.set_known_types(all_celltypes)
+path_memberships = paths_asym_bilat_analyzer.memberships()
+path_memberships = path_memberships[path_memberships.sum(axis=1)!=0]
+
+fraction_types_names = ['LHN', 'MB-FBN', 'CN', 'pre-dSEZ', 'pre-dVNC', 'RGN', 'dSEZ', 'dVNC', 'unknown']
+colors = ['#D4E29E', '#C144BC', '#8C7700', '#77CDFC','#E0B1AD', 'tab:purple','#D88052', '#A52A2A', 'tab:grey']
+ind = [x for x in range(0, len(path_memberships.columns))]
+
+fig, ax = plt.subplots(1,1, figsize=(3,2))
+
+# summary plot of 1st order upstream of dVNCs
+plts = []
+plt1 = plt.bar(ind, path_memberships.iloc[0,:], color = colors[0])
+bottom = path_memberships.iloc[0,:]
+plts.append(plt1)
+
+for i in range(1, len(path_memberships.index)):
+    plt1 = plt.bar(ind, path_memberships.iloc[i,:], bottom = bottom, color = colors[i])
+    bottom = bottom + path_memberships.iloc[i,:]
+    plts.append(plt1)
+
+plt.savefig('interhemisphere/plots/individual_asym_bilateral_partners.pdf', format='pdf', bbox_inches='tight')
+#plt.legend(plts, fraction_types_names)
+
+# %%
+# plot overview network all bilateral neurons
+# divided into 4 categories
+
+bilateral_cat = cos_list[cos_list.iou!=-1]
+len(bilateral_cat[bilateral_cat.iou<=0.1])
+len(bilateral_cat[(bilateral_cat.iou>0.1) & (bilateral_cat.iou<=0.25)])
+len(bilateral_cat[(bilateral_cat.iou>0.25) & (bilateral_cat.iou<=0.5)])
+len(bilateral_cat[(bilateral_cat.iou>0.5) & (bilateral_cat.iou<=0.75)])
+len(bilateral_cat[(bilateral_cat.iou>0.75) & (bilateral_cat.iou<=1)])
+
+cat1 = bilateral_cat[bilateral_cat.iou<=0.1]
+cat2 = bilateral_cat[(bilateral_cat.iou>0.1) & (bilateral_cat.iou<=0.25)]
+cat3 = bilateral_cat[(bilateral_cat.iou>0.25) & (bilateral_cat.iou<=0.5)]
+cat4 = bilateral_cat[(bilateral_cat.iou>0.5) & (bilateral_cat.iou<=0.75)]
+cat5 = bilateral_cat[(bilateral_cat.iou>0.75) & (bilateral_cat.iou<=1)]
+
+cats = [cat1, cat2, cat3, cat4, cat5]
+
+all_cats = []
+for i in range(len(cats)):
+    all_cats.append(Celltype(f'cat{i}_ipsi_partners', list(np.unique([x for sublist in cats[i].ipsi_ds_partners for x in sublist]))))
+    all_cats.append(Celltype(f'cat{i}_contra_partners', list(np.unique([x for sublist in cats[i].contra_ds_partners for x in sublist]))))
+
+all_cats_analyzer = Celltype_Analyzer(all_cats)
+all_cats_analyzer.set_known_types(all_celltypes)
+bilateral_cats_memberships = all_cats_analyzer.memberships()
+bilateral_cats_memberships = bilateral_cats_memberships[bilateral_cats_memberships.sum(axis=1)!=0]
+
+fraction_types_names = ['PN', 'LHN', 'MBIN', 'MBON','MB-FBN', 'CN', 'pre-dSEZ', 'pre-dVNC', 'RGN', 'dSEZ', 'dVNC', 'unknown']
+colors = ['#1D79B7', '#D4E29E', '#FF8734', '#F9EB4D', '#C144BC', '#8C7700', '#77CDFC','#E0B1AD', 'tab:purple','#D88052', '#A52A2A', 'tab:grey']
+ind = [x for x in range(0, len(bilateral_cats_memberships.columns))]
+
+fig, ax = plt.subplots(1,1, figsize=(3,2))
+
+# summary plot of 1st order upstream of dVNCs
+plts = []
+plt1 = plt.bar(ind, bilateral_cats_memberships.iloc[0,:], color = colors[0])
+bottom = bilateral_cats_memberships.iloc[0,:]
+plts.append(plt1)
+
+for i in range(1, len(bilateral_cats_memberships.index)):
+    plt1 = plt.bar(ind, bilateral_cats_memberships.iloc[i,:], bottom = bottom, color = colors[i])
+    bottom = bottom + bilateral_cats_memberships.iloc[i,:]
+    plts.append(plt1)
+
+#plt.legend(plts, fraction_types_names)
+plt.savefig('interhemisphere/plots/summary_bilateral_partners_5cats.pdf', format='pdf', bbox_inches='tight')
+
+
+cat1 = bilateral_cat[bilateral_cat.iou<=0.1]
+cat2 = bilateral_cat[(bilateral_cat.iou>0.1) & (bilateral_cat.iou<=0.25)]
+cat3 = bilateral_cat[(bilateral_cat.iou>0.25) & (bilateral_cat.iou<=1)]
+
+cats = [cat1, cat2, cat3]
+
+all_cats = []
+for i in range(len(cats)):
+    all_cats.append(Celltype(f'cat{i}_ipsi_partners', list(np.unique([x for sublist in cats[i].ipsi_ds_partners for x in sublist]))))
+    all_cats.append(Celltype(f'cat{i}_contra_partners', list(np.unique([x for sublist in cats[i].contra_ds_partners for x in sublist]))))
+
+all_cats_analyzer = Celltype_Analyzer(all_cats)
+all_cats_analyzer.set_known_types(all_celltypes)
+bilateral_cats_memberships = all_cats_analyzer.memberships()
+bilateral_cats_memberships = bilateral_cats_memberships[bilateral_cats_memberships.sum(axis=1)!=0]
+
+fraction_types_names = ['PN', 'LHN', 'MBIN', 'MBON','MB-FBN', 'CN', 'pre-dSEZ', 'pre-dVNC', 'RGN', 'dSEZ', 'dVNC', 'unknown']
+colors = ['#1D79B7', '#D4E29E', '#FF9852', '#F9EB4D', '#C144BC', '#8C7700', '#77CDFC','#E0B1AD', 'tab:purple','#C47451', '#A52A2A', 'tab:grey']
+ind = [x for x in range(0, len(bilateral_cats_memberships.columns))]
+
+fig, ax = plt.subplots(1,1, figsize=(1.5,2))
+
+# summary plot of 1st order upstream of dVNCs
+plts = []
+plt1 = plt.bar(ind, bilateral_cats_memberships.iloc[0,:], color = colors[0])
+bottom = bilateral_cats_memberships.iloc[0,:]
+plts.append(plt1)
+
+for i in range(1, len(bilateral_cats_memberships.index)):
+    plt1 = plt.bar(ind, bilateral_cats_memberships.iloc[i,:], bottom = bottom, color = colors[i])
+    bottom = bottom + bilateral_cats_memberships.iloc[i,:]
+    plts.append(plt1)
+
+plt.savefig('interhemisphere/plots/summary_bilateral_partners_3cats.pdf', format='pdf', bbox_inches='tight')
+
 # %%
