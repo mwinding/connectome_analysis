90f4218d7aa435ea11046b450c8241051c1bca35
mwinding <mwinding@alumni.nd.edu>
2021-03-04 20:38:01 +0000

    updated script to use new class

--- brain_completeness/pair_similarity/pair_stats_NBLAST.py
+++ brain_completeness/pair_similarity/pair_stats_NBLAST.py
@@ -1,18 +1,27 @@
 #%%
 import os
+import sys
 try:
     os.chdir('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
-    print(os.getcwd())
+    sys.path.append('/Volumes/GoogleDrive/My Drive/python_code/maggot_models/')
+    sys.path.append('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
 except:
     pass
 
-#%%
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
-import connectome_tools.process_matrix as promat
+import connectome_tools.process_matrix as pm
+
+# allows text to be editable in Illustrator
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+# font settings
+plt.rcParams['font.size'] = 12
+plt.rcParams['font.family'] = 'arial'
 
 # import R-to-L hemisphere NBLAST stats
 allNBLAST = pd.read_csv('data/Brain-NBLAST_R-to-L.csv', header = 0, index_col = 0, quotechar='"', skipinitialspace=True)
@@ -28,7 +37,7 @@ ranks = []
 for i in allNBLAST.index:
     if (sum(pairs.leftid==i)+sum(pairs.rightid==i) > 0 ):
         if(sum(allNBLAST.index==i)>0):
-            partner = promat.identify_pair(i, pairs)
+            partner = pm.Promat.identify_pair(i, pairs)
             if(sum(allNBLAST.columns==str(partner))>0):
                 score = allNBLAST.loc[i,:][str(partner)]
                 rowvalues = allNBLAST.loc[i,:]
@@ -43,13 +52,15 @@ for i in allNBLAST.index:
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
+binwidth = 1
+bins = np.arange(min(ranks), max(ranks) + binwidth*1.5) - binwidth*0.5
 fig, ax = plt.subplots(1,1,figsize=(.75,1.25))
 
-sns.distplot(ranks, bins=max(ranks), ax = ax, hist = True, kde = False, hist_kws=dict(edgecolor=(0, 0, 0, 1), linewidth=0.5), norm_hist=True)
+sns.distplot(ranks, bins=bins, ax = ax, hist = True, kde = False, hist_kws=dict(edgecolor=(0, 0, 0, 1), linewidth=0.5), norm_hist=True)
 #sns.distplot(values, ax = ax, hist = True, kde_kws = {'shade': True})
 
-ax.set(xlim = (1, 6))
-ax.set(ylim = (0, 0.8))
+ax.set(xlim = (0.5, 5.5))
+ax.set(ylim = (0, 1))
 ax.set(xticks=np.arange(1,6,1))
 ax.xaxis.set_tick_params(width=0.5)
 ax.yaxis.set_tick_params(width=0.5)
@@ -76,13 +87,15 @@ spectral_rank = sum(spectral_rank, [])
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
+binwidth = 1
+bins = np.arange(min(spectral_rank), max(spectral_rank) + binwidth*1.5) - binwidth*0.5
 fig, ax = plt.subplots(1,1,figsize=(.75,1.25))
 
-sns.distplot(spectral_rank, bins=6, ax = ax, hist = True, kde = False, color = sns.color_palette()[1] ,hist_kws=dict(edgecolor=(0, 0, 0, 1), linewidth=0.5), norm_hist=True)
+sns.distplot(spectral_rank, bins=bins, ax = ax, hist = True, kde = False, color = sns.color_palette()[1] ,hist_kws=dict(edgecolor=(0, 0, 0, 1), linewidth=0.5), norm_hist=True)
 #sns.distplot(values, ax = ax, hist = True, kde_kws = {'shade': True})
 
-ax.set(xlim = (1, 6))
-ax.set(ylim = (0, 0.8))
+ax.set(xlim = (0.5, 5.5))
+ax.set(ylim = (0, 1))
 ax.set(xticks=np.arange(1,6,1))
 ax.xaxis.set_tick_params(width=0.5)
 ax.yaxis.set_tick_params(width=0.5)
