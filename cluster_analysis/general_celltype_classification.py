f337638b086520ccdab505089dc36f42888a4c76
mwinding <mwinding@alumni.nd.edu>
2021-04-21 10:47:22 +0100

    testing new objects

--- cluster_analysis/general_celltype_classification.py
+++ cluster_analysis/general_celltype_classification.py
@@ -2,23 +2,19 @@
 import os
 try:
     os.chdir('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
-    print(os.getcwd())
 except:
     pass
 
-# %%
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pymaid as pymaid
-import glob as gl
+from pymaid_creds import url, name, password, token
+import pymaid
+import connectome_tools.cluster_analysis as clust
+import connectome_tools.celltype as ct
 
-# identify list of neuron-groups to import
-neuron_groups = gl.glob('data/color_iso-d=8/*.json')
+rm = pymaid.CatmaidInstance(url, token, name, password)
+celltypes = ct.Celltype_Analyzer.default_celltypes()
 
-# load skids of each neuron class
-clusters = map(lambda x : pd.read_json(x), neuron_groups)
-l_cluster = list(clusters)
 # %%
-df.skids[df.annotation == "sensory" | df.annotation == "motor"].unique()
