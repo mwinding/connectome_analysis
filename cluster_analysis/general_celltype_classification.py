6c881d4105305cdbb46e2a9ba64ab7dc6908a446
mwinding <mwinding@alumni.nd.edu>
2021-04-23 16:06:47 +0100

    testing new Celltype changes and membership plots

--- cluster_analysis/general_celltype_classification.py
+++ cluster_analysis/general_celltype_classification.py
@@ -1,7 +1,10 @@
 #%%
 import os
+import sys
 try:
     os.chdir('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
+    sys.path.append('/Volumes/GoogleDrive/My\ Drive/python_code/maggot_models/')
+    sys.path.append('/Volumes/GoogleDrive/My\ Drive/python_code/connectome_tools/')
 except:
     pass
 
@@ -15,6 +18,32 @@ import connectome_tools.cluster_analysis as clust
 import connectome_tools.celltype as ct
 
 rm = pymaid.CatmaidInstance(url, token, name, password)
-celltypes = ct.Celltype_Analyzer.default_celltypes()
 
+# allows text to be editable in Illustrator
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+# font settings
+plt.rcParams['font.size'] = 5
+plt.rcParams['font.family'] = 'arial'
+
+# %%
+
+ct.plot_cell_types_cluster('lvl7_labels', 'cluster_analysis/plots/celltypes-clusters.pdf')
+ct.plot_marginal_cell_type_cluster((2,1), ct.Celltype('MBONs', pymaid.get_skids_by_annotation('mw MBON')), 'green', 'lvl7_labels', 'cluster_analysis/plots/MBON_celltypes-clusters.pdf')
+
+# %%
+
+celltypes_data, celltypes = ct.Celltype_Analyzer.default_celltypes()
+in_hubs = ct.Celltype('In Hubs', pymaid.get_skids_by_annotation('mw hubs_in'), 'green')
+in_hubs.plot_cell_type_memberships(celltypes)
+
+# plot all hub-types at once
+in_hubs_ct = ct.Celltype('Out Hubs', pymaid.get_skids_by_annotation('mw hubs_out'), 'orange')
+out_hubs_ct = ct.Celltype('In Hubs', pymaid.get_skids_by_annotation('mw hubs_in'), 'green')
+in_out_hubs_ct = ct.Celltype('In-Out Hubs', pymaid.get_skids_by_annotation('mw hubs_in_out'), 'red')
+
+hubs = ct.Celltype_Analyzer([in_hubs_ct, in_out_hubs_ct, out_hubs_ct])
+hubs.set_known_types(celltypes)
+hubs.plot_memberships()
 # %%
