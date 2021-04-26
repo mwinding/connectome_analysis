9f89e56d6f35d058b4d4fc1e4a723abcf58ee8ed
mwinding <mwinding@alumni.nd.edu>
2021-04-26 09:53:50 +0100

    changed cwd modification

--- cluster_analysis/general_celltype_classification.py
+++ cluster_analysis/general_celltype_classification.py
@@ -1,12 +1,7 @@
 #%%
+
 import os
-import sys
-try:
-    os.chdir('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
-    sys.path.append('/Volumes/GoogleDrive/My\ Drive/python_code/maggot_models/')
-    sys.path.append('/Volumes/GoogleDrive/My\ Drive/python_code/connectome_tools/')
-except:
-    pass
+os.chdir(os.path.dirname(os.getcwd())) # make directory one step up the current directory
 
 import pandas as pd
 import numpy as np
