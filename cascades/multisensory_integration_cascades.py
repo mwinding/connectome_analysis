5718e3b1a16d9fac62ca519b8c35694a43c35797
mwinding <mwinding@alumni.nd.edu>
2021-05-19 20:36:22 +0100

    added upset plot break downs and boxenplot

--- cascades/multisensory_integration_cascades.py
+++ cascades/multisensory_integration_cascades.py
@@ -36,8 +36,8 @@ adj_ad = pm.Promat.pull_adj(type_adj='ad', subgraph='brain')
 
 #%%
 # pull sensory annotations and then pull associated skids
-order = ['ORN', 'AN sensories', 'MN sensories', 'photoreceptors', 'thermosensories', 'v\'td', 'A1 ascending noci', 'A1 ascending mechano', 'A1 ascending proprio', 'A1 ascending class II_III']
-sens = [ct.Celltype(name, pymaid.get_skids_by_annotation(f'mw {name}')) for name in order]
+order = ['olfactory', 'gustatory-external', 'gustatory-internal', 'enteric', 'thermo', 'visual', 'noci', 'mechano', 'proprio', 'touch', 'intero']
+sens = [ct.Celltype(name, ct.Celltype_Analyzer.get_skids_from_meta_annotation(f'mw {name}')) for name in order]
 input_skids_list = [x.get_skids() for x in sens]
 input_skids = [val for sublist in input_skids_list for val in sublist]
 
@@ -67,25 +67,132 @@ hops = 7
 sens_types = [ct.Celltype(hit_hist.get_name(), list(hit_hist.pairwise_threshold(threshold=threshold, hops=hops))) for hit_hist in input_hit_hist_list]
 sens_types_analyzer = ct.Celltype_Analyzer(sens_types)
 
-upset_threshold = 20
+upset_threshold = 30
 upset_threshold_dual_cats = 10
-path = f'cascades/plots/sens-cascades_upset_{upset_threshold}-threshold'
+path = f'cascades/plots/sens-cascades_upset'
 upset_members, members_selected, skids_excluded = sens_types_analyzer.upset_members(threshold=upset_threshold, path=path, plot_upset=True, 
                                                                                     exclude_singletons_from_threshold=True, exclude_skids=input_skids, threshold_dual_cats=upset_threshold_dual_cats)
-
 # check what's in the skids_excluded group
-# this group is assorted integrative that were excluded from the plot for simplicity
+# this group is assorted integrative that were excluded from the plot for simplicity; added manually as "other combinations" at far right of plot
 _, celltypes = ct.Celltype_Analyzer.default_celltypes()
-test_excluded = ct.Celltype_Analyzer([ct.Celltype('excluded25', skids_excluded)])
+test_excluded = ct.Celltype_Analyzer([ct.Celltype('excluded', skids_excluded)])
 test_excluded.set_known_types(celltypes)
-excluded_data = test_excluded.memberships()
+excluded_data = test_excluded.memberships(raw_num=True).drop(['sensories', 'ascendings'])
 
-# plot cell identity of all of these categories
+# cell identity of all of these categories
 upset_analyzer = ct.Celltype_Analyzer(members_selected)
 upset_analyzer.set_known_types(celltypes)
-upset_data = upset_analyzer.memberships() # the data is all out of order compared to upset plot
+upset_data = upset_analyzer.memberships(raw_num=True) # the data is all out of order compared to upset plot
 
-# plot cell identity of labelled line vs integrative
+# matched the order of the upset manually (couldn't find out quickly how to pull the data from the plot)
+# *** MANUAL INPUT REQUIRED HERE ***
+col_order = [9, 7, 6, 5, 4, 3, 1, 0, 10, 8, 2, 11, 12, 13, 14, 15, 16, 17]
+col_name_order = [upset_data.columns[i] for i in col_order]
+
+upset_data = upset_data.loc[:, col_name_order].drop(['sensories', 'ascendings'])
+
+# add set of miscellaneous integrations that is plotted as the final bar in the upset plot
+upset_data['+ all other combinations'] = excluded_data
+
+# identify columns of labelled line or integrative categories
+labelled_line = ['+' not in x for x in upset_data.columns]
+integrative = ['+' in x for x in upset_data.columns]
+vmax = 100
+
+#######
+# plot labelled line
+vmax=100
+cell_width=0.25
+# set up annotations to put on plot
+data = upset_data.loc[:, labelled_line]
+annotations = data.astype(str)
+annotations[annotations=='0']=''
+# generate heatmap
+fig, ax = plt.subplots(1,1, figsize=(cell_width * data.shape[0], cell_width * data.shape[1]))
+sns.heatmap(data, annot=annotations, fmt='s', square=True, cmap='Blues', vmax=vmax, ax=ax)
+plt.savefig('cascades/plots/sens-cascades_upset_celltype-members_labelled-line.pdf', format='pdf', bbox_inches='tight')
+
+#######
+# plot integrative
+# set up annotations to put on plot
+data = upset_data.loc[:, integrative]
+annotations = data.astype(str)
+annotations[annotations=='0']=''
+# generate heatmap
+fig, ax = plt.subplots(1,1, figsize=(cell_width * data.shape[0], cell_width * data.shape[1]))
+sns.heatmap(data, annot=annotations, fmt='s', square=True, cmap='Reds', vmax=vmax, ax=ax)
+plt.savefig('cascades/plots/sens-cascades_upset_celltype-members_integrative.pdf', format='pdf', bbox_inches='tight')
 
 # %%
 # plot hops from input labelled line vs integrative
+all_upset_cats = [members_selected[i] for i in col_order] + [ct.Celltype(name='+ all other combinations', skids=skids_excluded)]
+
+ll_upset_cats = [all_upset_cats[i] for i, x in enumerate(labelled_line) if x==True]
+int_upset_cats = [all_upset_cats[i] for i, x in enumerate(integrative) if x==True]
+
+# determine median hops from sensory for labelled-line neurons
+neuron_data_ll = []
+for cat in ll_upset_cats:
+    sens_type = cat.get_name()
+    modality = [hit_hist for hit_hist in input_hit_hist_list if sens_type==hit_hist.get_name()][0]
+    skid_hit_hist = modality.get_skid_hit_hist()
+    for skid in cat.get_skids():
+        data = skid_hit_hist.loc[skid, :]
+
+        # convert to counts
+        count=[]
+        for i in data.index:
+            num = int(data.iloc[i])
+            if(num>0): count = count + [i]*num
+
+        neuron_data_ll.append([skid, np.median(count), sens_type, 'labelled-line'])
+
+# determine median hops from sensory for integrative neurons
+neuron_data_int = []
+pairList = pm.Promat.get_pairs()
+for cat in int_upset_cats:
+    for hit_hist in input_hit_hist_list:
+        skid_hit_hist = hit_hist.get_skid_hit_hist()
+        for skid in cat.get_skids():
+
+            # identify pair to check pairwise threshold for each sensory modality
+            partner = pm.Promat.identify_pair(skid, pairList)
+
+            # for paired neurons (partner skid != skid for paired neurons)
+            if(partner!=skid):
+                data = skid_hit_hist.loc[skid, :]
+                partner_data = skid_hit_hist.loc[partner, :]
+                if((sum(data)+sum(partner_data))>=n_init): # check pairwise threshold again here
+                    # convert to counts
+                    count=[]
+                    for i in data.index:
+                        num = int(data.iloc[i])
+                        if(num>0): count = count + [i]*num
+
+            # for nonpaired neurons
+            if(partner==skid):
+                data = skid_hit_hist.loc[skid, :]
+                if(sum(data)>=n_init/2): # check threshold again here for unpaired
+                    # convert to counts
+                    count=[]
+                    for i in data.index:
+                        num = int(data.iloc[i])
+                        if(num>0): count = count + [i]*num
+
+                neuron_data_int.append([skid, np.median(count), hit_hist.get_name(), 'integrative'])        
+
+df_hops = pd.DataFrame(neuron_data_ll + neuron_data_int, columns=['skid', 'hops', 'sensory', 'type'])
+df_hops = df_hops.set_index(['type', 'skid', 'sensory'])
+
+# boxenplot of hops from sens/ascending neurons for integrative vs. labelled line
+df_hops['hops'] = df_hops['hops']-0.5 # makes the boxenplot line up with each hop level
+fig, ax = plt.subplots(1,1, figsize=(1,1.5))
+sns.boxenplot(data=df_hops, x='type', y='hops', k_depth='full', orient='v', linewidth=0.5, ax=ax)
+plt.savefig('cascades/plots/sens-cascades_hops_plot.pdf', format='pdf', bbox_inches='tight')
+df_hops['hops'] = df_hops['hops']+0.5 # revert back to normal
+
+# %%
+# take the upset categories and make labelled-line / integrative plot of clusters
+
+# %%
+# labelled-line / integrative plot of sensory neuropils
