ef283413125f1a2c3220877eac37c24a913318d9
mwinding <mwinding@alumni.nd.edu>
2021-07-15 19:13:01 +0100

    updated script with new methods

--- descending_neurons_analysis/projectome_format.py
+++ descending_neurons_analysis/projectome_format.py
@@ -1,16 +1,6 @@
 #%%
 import os
-
-try:
-    os.chdir('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
-    print(os.getcwd())
-except:
-
-    pass
-
-#%%
-import sys
-sys.path.append("/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/")
+os.chdir(os.path.dirname(os.getcwd())) # make directory one step up the current directory
 
 import pandas as pd
 import numpy as np
@@ -19,21 +9,21 @@ import seaborn as sns
 from tqdm import tqdm
 import pymaid
 from pymaid_creds import url, name, password, token
-
-projectome = pd.read_csv('data/projectome_2_7_2020.csv')
 rm = pymaid.CatmaidInstance(url, token, name, password)
+import connectome_tools.process_matrix as pm
+
+projectome = pd.read_csv('data/projectome/projectome_mw brain paper all neurons_split.csv', header = 0)
 
 # %%
 # format into adjacency matrix
-#unique_skids = list(map(str, np.unique(projectome['skeleton'])))
-unique_skids = np.unique(projectome['skeleton']).tolist()
-meshes = projectome.columns[7:33].tolist()
+skids = np.unique(projectome['skeleton']).tolist()
+meshes = projectome.columns[8:34].tolist()
 meshes.sort()
 meshes = meshes[16:20] + meshes[20:26] + meshes[0:16]
 
 # projectome matrix
-zero_data = np.zeros(shape=(len(meshes) + len(unique_skids),len(meshes) + len(unique_skids)))
-project_mat = pd.DataFrame(zero_data, columns=(unique_skids + meshes), index = (unique_skids + meshes))
+zeros_array = np.zeros(shape=(len(meshes) + len(skids),len(meshes) + len(skids)))
+project_mat = pd.DataFrame(zeros_array, columns=(skids + meshes), index = (skids + meshes))
 
 no_mesh_inputs = 0
 no_mesh_outputs = 0
@@ -42,8 +32,9 @@ multi_mesh_outputs = 0
 
 # add num outputs here?
 # currently just sums up presynaptic sites
-for skeleton in tqdm(unique_skids):
-    skel_projectome = projectome.loc[projectome.skeleton == skeleton, :]
+for skid in tqdm(skids):
+    
+    skel_projectome = projectome.loc[projectome.skeleton == skid, :]
     skel_projectome = skel_projectome.reset_index()
     #print(skeleton)
     for i in range(0, len(skel_projectome.index)):
@@ -53,55 +44,31 @@ for skeleton in tqdm(unique_skids):
                 # how many outputs from connector?
                 skel_projectome.iloc[i, :].connector 
                 mesh = location[location==1].index.tolist()[0]
-                project_mat.loc[skeleton, mesh] = project_mat.loc[skeleton, mesh] + 1
+                project_mat.loc[skid, mesh] = project_mat.loc[skid, mesh] + 1
 
             if(sum(location) > 1):
-                #print('>1 meshes for output connector %i from skeleton %i' %(skel_projectome.connector[i], skeleton))
+                #print('>1 meshes for output connector %i from skid %i' %(skel_projectome.connector[i], skid))
                 multi_mesh_outputs = multi_mesh_outputs + 1
 
             if(sum(location) == 0):
-                #print('0 meshes for output connector %i from skeleton %i' %(skel_projectome.connector[i], skeleton))
+                #print('0 meshes for output connector %i from skid %i' %(skel_projectome.connector[i], skid))
                 no_mesh_outputs = no_mesh_outputs + 1
 
         if(projectome['is_input'][i]==1):
             if(sum(location) == 1): # inputs from only one mesh
                 mesh = location[location==1].index.tolist()[0]
-                project_mat.loc[mesh, skeleton] = project_mat.loc[mesh, skeleton] + 1
+                project_mat.loc[mesh, skid] = project_mat.loc[mesh, skid] + 1
 
             if(sum(location) > 1):
-                #print('>1 meshes for input connector %i from skeleton %i' %(skel_projectome.connector[i], skeleton))
+                #print('>1 meshes for input connector %i from skid %i' %(skel_projectome.connector[i], skid))
                 multi_mesh_inputs = multi_mesh_inputs + 1
 
             if(sum(location) == 0):
-                #print('0 meshes for input connector %i from skeleton %i' %(skel_projectome.connector[i], skeleton))
+                #print('0 meshes for input connector %i from skid %i' %(skel_projectome.connector[i], skid))
                 no_mesh_inputs = no_mesh_inputs + 1
 # %%
-# identify skeleton ID of hemilateral neuron pair, based on CSV pair list
+# export adjacency to csv
 
-dVNC = pymaid.get_skids_by_annotation('mw dVNC')
-pairs = pd.read_csv('data/pairs-2020-05-08.csv', header = 0)
-
-pairOrder = []
-for skid in dVNC:
-    if(skid in pairs["leftid"].values):
-        pair_skid = pairs["rightid"][pairs["leftid"]==skid].iloc[0]
-        pairOrder.append(skid)
-        pairOrder.append(pair_skid)
-
-# %%
-fig, ax = plt.subplots(1,1,figsize=(10,10))
-#sns.heatmap(matrix, ax = ax, cmap='OrRd')
-
-plt.imshow(project_mat.loc[pairOrder, meshes[2:len(meshes)]], cmap='OrRd', vmax = 20)
-# %%
-fig, ax = plt.subplots(1,1,figsize=(10,10))
-#sns.heatmap(matrix, ax = ax, cmap='OrRd')
-
-plt.imshow(project_mat.loc[meshes[2:len(meshes)], pairOrder], cmap='OrRd', vmax = 20)
-
-# %%
-# export 
-
-project_mat.to_csv('descending_neurons_analysis/data/projectome_adjacency.csv')
+project_mat.to_csv('data/projectome/projectome_adjacency.csv')
 
 # %%
