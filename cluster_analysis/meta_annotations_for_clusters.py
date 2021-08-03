e9464588c854a686fdce0f5c98f88f4a4a9d6e32
mwinding <mwinding@alumni.nd.edu>
2021-08-03 14:43:33 +0100

    initial commit to write brain cluster annotations

---
+++ cluster_analysis/meta_annotations_for_clusters.py
@@ -0,0 +1,54 @@
+#%%
+import os
+os.chdir(os.path.dirname(os.getcwd())) # make directory one step up the current directory
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pymaid_creds import url, name, password, token
+import pymaid
+rm = pymaid.CatmaidInstance(url, token, name, password)
+
+import connectome_tools.celltype as ct
+import connectome_tools.process_graph as pg
+import connectome_tools.process_matrix as pm
+import connectome_tools.process_skeletons as skel
+import connectome_tools.cluster_analysis as clust
+
+# %%
+# set up annotation names and organize clusters
+
+cluster_lvls = [0,1,2,3,4,5,6,7]
+clusters = [clust.Analyze_Cluster(x).cluster_df for x in cluster_lvls]
+annot_names = [list(map(lambda x: (f'{x[0]}_clusterID-{x[1]:.0f}_walksort-{x[2]:.3f}'), zip(clust.index, clust.cluster, clust.sum_walk_sort))) for clust in clusters]
+
+for i in range(len(clusters)):
+    clusters[i]['annot'] = annot_names[i]
+
+# switch 15672263 to 21790197
+for i in range(len(clusters)):
+    skids_to_add = []
+    for j in range(len(clusters[i].index)):
+        skids_to_check = list(clusters[i].loc[j, 'skids'])
+        skids_checked = [x if x!=15672263 else 21790197 for x in skids_to_check] # swapping for other skid
+        skids_to_add.append(skids_checked) # adding to list of lists
+
+    clusters[i]['skids'] = skids_to_add # replace old sets of skids with one with swapped skid
+
+# %%
+# writing annotations for clusters
+
+for i in range(len(clusters)):
+    for j in range(len(clusters[i].index)):
+        annot_skids = clusters[i].loc[j, 'skids']
+        annot = clusters[i].loc[j, 'annot']
+        pymaid.add_annotations(annot_skids, [annot])
+
+    annots = list(clusters[i].annot)
+    pymaid.add_meta_annotations(annots, f'mw brain clusters level {i}')
+
+pymaid.add_meta_annotations([f'mw brain clusters level {lvl}' for lvl in cluster_lvls], 'mw brain clusters')
+
+# %%
+#
