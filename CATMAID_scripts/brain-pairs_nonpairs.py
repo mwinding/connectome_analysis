96f113aba40f2910a64add664798f90f9d1a8ffc
mwinding <mwinding@alumni.nd.edu>
2021-11-04 15:39:04 +0000

    initial commit; export pair IDs in brain

---
+++ CATMAID_scripts/brain-pairs_nonpairs.py
@@ -0,0 +1,28 @@
+#%%
+import os
+import sys
+os.chdir(os.path.dirname(os.getcwd())) # make directory one step up the current directory
+
+from pymaid_creds import url, name, password, token
+import pymaid
+rm = pymaid.CatmaidInstance(url, token, name, password)
+
+import numpy as np
+import pandas as pd
+import connectome_tools.process_matrix as pm
+
+pairs = pm.Promat.get_pairs()
+
+# %%
+# identify brain neurons in pair-list; brain neurons not in pair-list
+# output as CSVs
+
+brain = pymaid.get_skids_by_annotation('mw brain neurons')
+
+brain_pairs = pd.DataFrame([list(x) for x in pairs.values if x[0] in brain], columns=['left', 'right'])
+brain_unpaired = pd.DataFrame(np.setdiff1d(brain, list(brain_pairs.left) + list(brain_pairs.right)), columns=['unpaired'])
+
+brain_pairs.to_csv('data/pairs/brain-pairs.csv')
+brain_unpaired.to_csv('data/pairs/brain-unpaired.csv')
+
+# %%
