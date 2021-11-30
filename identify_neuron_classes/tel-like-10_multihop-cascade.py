a357808a4b4d974e04e32fb511a6188e779cc8dc
mwinding <mwinding@alumni.nd.edu>
2021-11-30 13:01:44 +0000

    initial commit for connectivity demo for Nadine

---
+++ identify_neuron_classes/tel-like-10_multihop-cascade.py
@@ -0,0 +1,40 @@
+# %%
+import os
+os.chdir(os.path.dirname(os.getcwd())) # make directory one step up the current directory
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pymaid_creds import url, name, password, token
+import pymaid
+rm = pymaid.CatmaidInstance(url, token, name, password)
+
+import connectome_tools.process_matrix as pm
+import navis
+
+# allows text to be editable in Illustrator
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+# font settings
+plt.rcParams['font.size'] = 5
+plt.rcParams['font.family'] = 'arial'
+
+# %%
+neurons = pymaid.get_skids_by_annotation('nr Tel-like 10') # Tel-like 10
+
+# use pregenerated edge list
+edges = pd.read_csv('data/edges_threshold/pairwise-threshold_ad_all-edges.csv', index_col=0)
+
+# downstream 3-hops of Tel-like 10
+downstream = pm.Promat.downstream_multihop(edges=edges, sources=neurons, hops=3)
+[pymaid.add_annotations(skids, f'nr Tel-like 10 downstream {i+1}-hop') for i, skids in enumerate(downstream)]
+
+# identify dVNcs in 3-hops downstream of Tel-like 10
+dVNC = pymaid.get_skids_by_annotation('mw dVNC')
+downstream_dVNCs = [list(np.intersect1d(dVNC, skids)) for skids in downstream]
+[pymaid.add_annotations(skids, f'nr Tel-like 10 downstream-dVNCs {i+1}-hop') for i, skids in enumerate(downstream_dVNCs)]
+
+
+# %%
