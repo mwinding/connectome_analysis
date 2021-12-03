0140344876aa62d84200419c74809658a69c54fd
mwinding <mwinding@alumni.nd.edu>
2021-12-03 17:48:44 +0000

    initial commit

---
+++ cascades/feedback_through_brain/single-cell_recurrence.py
@@ -0,0 +1,161 @@
+#%%
+import sys
+import os
+
+os.chdir(os.path.dirname(os.getcwd())) # make directory one step up the current directory
+
+from pymaid_creds import url, name, password, token
+import pymaid
+rm = pymaid.CatmaidInstance(url, token, name, password)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import cmasher as cmr
+
+# allows text to be editable in Illustrator
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+# font settings
+plt.rcParams['font.size'] = 5
+plt.rcParams['font.family'] = 'arial'
+
+import connectome_tools.cascade_analysis as casc
+import connectome_tools.celltype as ct
+import connectome_tools.process_matrix as pm
+import connectome_tools.cluster_analysis as clust
+
+import pickle 
+
+n_init = 1000
+pair_hist_list = pickle.load(open(f'data/cascades/all-brain-pairs_outputs-added_{n_init}-n_init.p', 'rb'))
+
+# %%
+# identify ds_partners
+from tqdm import tqdm
+from joblib import Parallel, delayed
+
+threshold = n_init/2
+hops = 8
+pairs = pm.Promat.get_pairs()
+
+'''
+def find_ds_partner_ids(pair_hist, pair_hist_list, pairs):
+    ds_partners = list(pair_hist.pairwise_threshold(threshold=threshold, hops=hops))
+    pair_ids = pm.Promat.load_pairs_from_annotation('ds', pairs, skids=ds_partners, use_skids=True)
+    pair_ids = list(pair_ids.leftid)
+    pair_ids = list(np.intersect1d(pair_ids, [x.name for x in pair_hist_list]))
+    return(pair_ids)
+
+ds_partners = Parallel(n_jobs=-1)(delayed(find_ds_partner_ids)(pair_hist_list[i], pair_hist_list, pairs) for i in tqdm(range(len(pair_hist_list))))
+pickle.dump(ds_partners, open(f'data/cascades/all-brain-pairs_ds_partners_{n_init}-n_init.p', 'wb'))
+'''
+
+ds_partners = pickle.load(open(f'data/cascades/all-brain-pairs_ds_partners_{n_init}-n_init.p', 'rb'))
+ds_partners_df = pd.DataFrame(list(map(lambda x: [x[0], x[1]], zip([x.name for x in pair_hist_list], ds_partners))), columns=['skid', 'ds_partners'])
+ds_partners_df.set_index('skid', inplace=True)
+# %%
+# how many partners are in recurrent loops?
+
+# collect all recurrent skids
+recurrent_partners_col = []
+for skid in ds_partners_df.index:
+    recurrent_partners = []
+    ds_partners = ds_partners_df.loc[skid, 'ds_partners']
+    for partner in ds_partners:
+        ds_ds_partners = ds_partners_df.loc[partner, 'ds_partners']
+        if(skid in ds_ds_partners):
+            recurrent_partners.append(partner)
+
+    recurrent_partners_col.append(recurrent_partners)
+
+# fraction of recurrent vs. nonrecurrent parents
+ds_partners_df['recurrent_partners'] = recurrent_partners_col
+frac_recurrent = [len(ds_partners_df.loc[i, 'recurrent_partners'])/len(ds_partners_df.loc[i, 'ds_partners']) if len(ds_partners_df.loc[i, 'ds_partners'])>0 else 0 for i in ds_partners_df.index]
+ds_partners_df['fraction_recurrent_partners'] = frac_recurrent
+
+# stripplot of data
+fig, ax = plt.subplots(1,1,figsize=(4,4))
+sns.stripplot(y=ds_partners_df.fraction_recurrent_partners, ax=ax, s=3, alpha=0.5)
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions.pdf', format='pdf', bbox_inches='tight')
+
+# distribution plot
+fig, ax = plt.subplots(1,1,figsize=(4,4))
+sns.histplot(y=ds_partners_df.fraction_recurrent_partners, binwidth=0.05, ax=ax)
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions_hist.pdf', format='pdf', bbox_inches='tight')
+
+# %%
+# by cell type and cluster
+
+_, celltypes = ct.Celltype_Analyzer.default_celltypes()
+all_celltypes = [x.skids for x in celltypes]
+all_celltypes = [x for sublist in all_celltypes for x in sublist]
+other_ct = ct.Celltype('Other', np.setdiff1d(pymaid.get_skids_by_annotation('mw brain neurons'), all_celltypes), 'tab:gray')
+celltypes = celltypes + [other_ct]
+
+celltype_annotation = []
+for skid in ds_partners_df.index:
+    for celltype in celltypes:
+        if(skid in celltype.skids):
+            celltype_annotation.append(celltype.name)
+
+ds_partners_df['celltype'] = celltype_annotation
+
+# plot results as barplot with points, barplot, or violinplot
+fig, ax = plt.subplots(1,1,figsize=(4,4))
+sns.barplot(x=ds_partners_df.celltype, y=ds_partners_df.fraction_recurrent_partners, order=['PNs', 'PNs-somato', 'LNs', 'LHNs', 'FFNs', 'MBINs', 'MBONs', 'MB-FBNs', 'CNs', 'pre-dSEZs', 'pre-dVNCs', 'dSEZs', 'dVNCs', 'Other'])
+sns.stripplot(x=ds_partners_df.celltype, y=ds_partners_df.fraction_recurrent_partners, s=1, alpha=0.5, color='black', order=['PNs', 'PNs-somato', 'LNs', 'LHNs', 'FFNs', 'MBINs', 'MBONs', 'MB-FBNs', 'CNs', 'pre-dSEZs', 'pre-dVNCs', 'dSEZs', 'dVNCs', 'Other'])
+plt.xticks(rotation=45, ha='right')
+ax.set(ylim=(-0.05, 1))
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions_by-celltype_barplot-with-points.pdf', format='pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots(1,1,figsize=(4,4))
+sns.barplot(x=ds_partners_df.celltype, y=ds_partners_df.fraction_recurrent_partners, order=['PNs', 'PNs-somato', 'LNs', 'LHNs', 'FFNs', 'MBINs', 'MBONs', 'MB-FBNs', 'CNs', 'pre-dSEZs', 'pre-dVNCs', 'dSEZs', 'dVNCs', 'Other'])
+plt.xticks(rotation=45, ha='right')
+ax.set(ylim=(0, 1))
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions_by-celltype_barplot.pdf', format='pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots(1,1,figsize=(8,4))
+sns.violinplot(x=ds_partners_df.celltype, y=ds_partners_df.fraction_recurrent_partners, scale='width', order=['PNs', 'PNs-somato', 'LNs', 'LHNs', 'FFNs', 'MBINs', 'MBONs', 'MB-FBNs', 'CNs', 'pre-dSEZs', 'pre-dVNCs', 'dSEZs', 'dVNCs', 'Other'])
+plt.xticks(rotation=45, ha='right')
+ax.set(ylim=(0, 1))
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions_by-celltype_violinplot.pdf', format='pdf', bbox_inches='tight')
+
+# %%
+# recurrent in clusters
+
+clusters = ct.Celltype_Analyzer.get_skids_from_meta_annotation('mw brain clusters level 7', split=True)
+clusters_ct = list(map(lambda x: ct.Celltype(*x), zip(clusters[1], clusters[0])))
+
+cluster_annotation = []
+for skid in ds_partners_df.index:
+    i=0
+    for celltype in clusters_ct:
+        if(skid in celltype.skids):
+            cluster_annotation.append(celltype.name)
+        if(skid not in celltype.skids):
+            i+=1
+        if(i==90):
+            cluster_annotation.append('None')
+
+
+ds_partners_df['cluster'] = cluster_annotation
+
+
+# plot results as barplot with points, barplot, or violinplot
+fig, ax = plt.subplots(1,1,figsize=(8,4))
+sns.barplot(x=ds_partners_df.cluster, y=ds_partners_df.fraction_recurrent_partners, order=[x.name for x in clusters_ct])
+sns.stripplot(x=ds_partners_df.cluster, y=ds_partners_df.fraction_recurrent_partners, s=1, alpha=0.5, color='black', order=[x.name for x in clusters_ct])
+plt.xticks(rotation=45, ha='right')
+ax.set(ylim=(-0.05, 1))
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions_by-cluster_barplot-with-points.pdf', format='pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots(1,1,figsize=(8,4))
+sns.barplot(x=ds_partners_df.cluster, y=ds_partners_df.fraction_recurrent_partners, order=[x.name for x in clusters_ct])
+plt.xticks(rotation=45, ha='right')
+ax.set(ylim=(0, 1))
+plt.savefig('cascades/feedback_through_brain/plots/recurrent-partner-fractions_by-cluster_barplot.pdf', format='pdf', bbox_inches='tight')
+
+# %%
