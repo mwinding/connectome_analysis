7837aa50b361d9bba94667d450d8f85575e02e7c
mwinding <mwinding@alumni.nd.edu>
2022-01-21 11:19:55 +0000

    added generic names

--- CATMAID_scripts/deannotate_using_meta_annotation.py
+++ CATMAID_scripts/deannotate_using_meta_annotation.py
@@ -14,12 +14,13 @@ rm = pymaid.CatmaidInstance(url, token, name, password)
 # deannotation all sub-annotations under meta-annotation
 ## VERY DANGEROUS; BE CAREFUL!!! ##
 
-meta = 'mw partner-loops-bilateral partners'
+meta = 'meta-something'
 annots = [annot for annot in pymaid.get_annotated(meta).name]
 
 [pymaid.remove_annotations(pymaid.get_skids_by_annotation(annot), annot) for annot in annots]
 # %%
 # dennotate single annotation
 
-annot = 'mw partner loops'
+annot = 'something'
 pymaid.remove_annotations(pymaid.get_skids_by_annotation(annot), annot)
+# %%
