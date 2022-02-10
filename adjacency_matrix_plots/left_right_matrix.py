1d05b1f34f7ba7743cfcbac103d5c635fda1aeff
mwinding <mwinding@alumni.nd.edu>
2022-02-10 10:50:17 +0000

    remove commented out section

--- adjacency_matrix_plots/left_right_matrix.py
+++ adjacency_matrix_plots/left_right_matrix.py
@@ -4,18 +4,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import math
-#from pymaid_creds import url, name, password, token
-#import pymaid
 
-#rm = pymaid.CatmaidInstance(url, token, name, password)
-#left = pymaid.get_skids_by_annotation('mw left')
-#right = pymaid.get_skids_by_annotation('mw right')
-'''
-
-fig, ax = plt.subplots(1,1,figsize=(4,4))
-sns.heatmap(matrix, ax = ax, cmap='OrRd')
-plt.savefig('adjacency_matrix/plots/matrix.pdf', bbox_inches='tight', transparent = True)
-'''
 # %%
 #matrix = np.random.randint(50, size=(2500, 2500))
 matrix = pd.read_csv('data/G-pair-sorted.csv', header = 0, index_col = 0)
