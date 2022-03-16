e83a9731c0acec5410ed5606a7fce7db293bb1ae
Michael Winding <mwinding@alumni.nd.edu>
2022-03-16 16:23:36 +0000

    added code chunks

--- generate_data/cascades_all-brain-pairs.py
+++ generate_data/cascades_all-brain-pairs.py
@@ -1,3 +1,4 @@
+# %%
 # generate cascades from individual neuron pairs and individual nonpaired neurons
 
 from data_settings import data_date, pairs_path
@@ -12,7 +13,7 @@ import pickle
 from contools import Promat, Adjacency_matrix, Celltype_Analyzer, Celltype, Cascade_Analyzer
 
 today_date = '2022-03-15'
-
+# %%
 # load adjacency matrix for cascades
 subgraph = ['mw brain paper clustered neurons', 'mw brain accessory neurons']
 adj_ad = Promat.pull_adj(type_adj='ad', date=data_date, subgraph=subgraph)
@@ -28,6 +29,7 @@ brain_pair_list = brain_pair_list + brain_nonpaired_list
 output_skids = Celltype_Analyzer.get_skids_from_meta_annotation('mw brain outputs')
 output_skids = output_skids + pymaid.get_skids_by_annotation('mw motor')
 
+# %%
 # cascades from each neuron pair
 # save as pickle to use later because cascades are stochastic; prevents the need to remake plots everytime
 p = 0.05
@@ -37,14 +39,12 @@ simultaneous = True
 adj=adj_ad
 
 source_names = [x[0] for x in brain_pair_list]
-
-source_names = source_names[0:3]
-brain_pair_list = brain_pair_list[0:3]
 pair_hist_list = Cascade_Analyzer.run_cascades_parallel(source_skids_list=brain_pair_list, source_names = source_names, stop_skids=output_skids, 
                                                                     adj=adj_ad, p=p, max_hops=max_hops, n_init=n_init, simultaneous=simultaneous)
 
 pickle.dump(pair_hist_list, open(f'data/cascades/all-brain-pairs_{n_init}-n_init_{today_date}.p', 'wb'))
-'''
+
+# %%
 # cascades from descending neurons
 # in the above, since descending neurons are stop nodes, they effectively don't emit signal
 output_pairs, _, output_nonpaired = Promat.extract_pairs_from_list(Celltype_Analyzer.get_skids_from_meta_annotation('mw brain outputs'), Promat.get_pairs(pairs_path))
@@ -63,4 +63,5 @@ for i in range(0, len(output_hist_list)):
             pair_hist_list[j] = output_hist_list[i]
 
 pickle.dump(pair_hist_list, open(f'data/cascades/all-brain-pairs_outputs-added_{n_init}-n_init.p', 'wb'))
-'''
+
+# %%
