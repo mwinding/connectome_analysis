ad6ce76df1abf63535898a37b7da65b77502a1e7
Michael Winding <mwinding@alumni.nd.edu>
2022-05-10 12:26:59 +0100

    added projectome dataset date

--- data_settings.py
+++ data_settings.py
@@ -1,3 +1,4 @@
 pairs_path = 'data/pairs/pairs-2022-02-14.csv'
 data_date = '2022-03-15' # dataset of brain and inputs
 data_date_A1_brain = '2022-03-17' # dataset of brain, brain inputs, A1, and A1 inputs
+data_date_projectome = '2022-05-05'
