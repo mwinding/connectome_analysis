7f09afbe1d3b8ddd397ef08b06d1a799a9c122f1
Michael Winding <mwinding@alumni.nd.edu>
2022-05-20 14:46:57 +0100

    removed reference to local dir

--- scripts/brain_completeness/brain_simple-synaptic-completeness.py
+++ scripts/brain_completeness/brain_simple-synaptic-completeness.py
@@ -1,11 +1,4 @@
 #%%
-import os
-import sys
-try:
-    os.chdir('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
-    sys.path.append('/Volumes/GoogleDrive/My Drive/python_code/connectome_tools/')
-except:
-    pass
 
 import numpy as np
 import pandas as pd
