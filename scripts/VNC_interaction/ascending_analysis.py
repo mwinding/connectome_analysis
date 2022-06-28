80c24cd094c2b84db4ea5a81be1f2da99ebd0a0b
Michael Winding <mwinding@alumni.nd.edu>
2022-06-28 15:23:10 +0100

    updated to work with new classes

--- scripts/VNC_interaction/ascending_analysis.py
+++ scripts/VNC_interaction/ascending_analysis.py
@@ -1,6 +1,6 @@
 # %%
-
 from pymaid_creds import url, name, password, token
+from data_settings import data_date, data_date_A1_brain, pairs_path
 import pymaid
 rm = pymaid.CatmaidInstance(url, token, name, password)
 import navis
@@ -9,19 +9,82 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import connectome_tools.process_matrix as pm
+from contools import Promat, Celltype, Celltype_Analyzer
 
 # allows text to be editable in Illustrator
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
 rm = pymaid.CatmaidInstance(url, token, name, password)
-adj = pd.read_csv('VNC_interaction/data/axon-dendrite.csv', header = 0, index_col = 0)
-inputs = pd.read_csv('VNC_interaction/data/input_counts.csv', index_col = 0)
-inputs = pd.DataFrame(inputs.values, index = inputs.index, columns = ['axon_input', 'dendrite_input'])
-pairs = pm.Promat.get_pairs() # import pairs
+select_neurons = pymaid.get_skids_by_annotation(['mw A1 neurons paired', 'mw dVNC'])
+select_neurons = select_neurons + Celltype_Analyzer.get_skids_from_meta_annotation('mw A1 sensories')
+ad_edges_A1 = Promat.pull_edges(type_edges='ad', threshold=0.01, data_date=data_date_A1_brain, pairs_combined=False, select_neurons=select_neurons)
+pairs = Promat.get_pairs(pairs_path=pairs_path)
 
-sens_asc_mat_thresh = pd.read_csv('VNC_interaction/plots/individual_asc_paths/ascending_identity_2-hops.csv', header=0, index_col=0)
+# load sensory types
+A1_MN = pymaid.get_skids_by_annotation('mw A1 MN')
+
+A1_proprio = pymaid.get_skids_by_annotation('mw A1 proprio')
+A1_chordotonal = pymaid.get_skids_by_annotation('mw A1 chordotonals')
+A1_noci = pymaid.get_skids_by_annotation('mw A1 noci')
+A1_classII_III = pymaid.get_skids_by_annotation('mw A1 classII_III')
+A1_external = pymaid.get_skids_by_annotation('mw A1 external sensories')
+A1_unk = pymaid.get_skids_by_annotation('mw A1 unknown sensories')
+dVNC = pymaid.get_skids_by_annotation('mw dVNC')
+
+# determining hops from each sensory modality for each ascending neuron (using all hops)
+names = ['us-MN', 'ds-Proprio', 'ds-Noci', 'ds-Chord', 'ds-ClassII_III', 'ds-ES', 'ds-unk']
+general_names = ['pre-MN', 'Proprio', 'Noci', 'Chord', 'ClassII_III', 'ES', 'unk']
+exclude = A1_MN + A1_proprio + A1_chordotonal + A1_noci + A1_classII_III + A1_external + A1_unk + dVNC
+
+hops = 5
+us_MN = Promat.upstream_multihop(edges=ad_edges_A1, sources=A1_MN, hops=hops, exclude=exclude, pairs=pairs)
+ds_proprio = Promat.downstream_multihop(edges=ad_edges_A1, sources=A1_proprio, hops=hops, exclude=exclude, pairs=pairs)
+ds_noci = Promat.downstream_multihop(edges=ad_edges_A1, sources=A1_noci, hops=hops, exclude=exclude, pairs=pairs)
+ds_chord = Promat.downstream_multihop(edges=ad_edges_A1, sources=A1_chordotonal, hops=hops, exclude=exclude, pairs=pairs)
+ds_classII_III = Promat.downstream_multihop(edges=ad_edges_A1, sources=A1_classII_III, hops=hops, exclude=exclude, pairs=pairs)
+ds_external = Promat.downstream_multihop(edges=ad_edges_A1, sources=A1_external, hops=hops, exclude=exclude, pairs=pairs)
+ds_unk = Promat.downstream_multihop(edges=ad_edges_A1, sources=A1_unk, hops=hops, exclude=exclude, pairs=pairs)
+
+VNC_layers = [us_MN, ds_proprio, ds_noci, ds_chord, ds_classII_III, ds_external, ds_unk]
+cat_order = ['pre-MN', 'Proprio', 'Noci', 'Chord', 'ClassII_III', 'ES', 'Unknown']
+
+# %%
+# identities of ascending neurons
+
+# load ascending and sort by modality
+A1_ascending = Celltype_Analyzer.get_skids_from_meta_annotation('mw A1 ascending', split=True)
+ascending_pairs = [Promat.extract_pairs_from_list(skids, pairs)[0] for skids in A1_ascending[0]]
+
+for i in range(len(ascending_pairs)):
+    ascending_pairs[i]['type'] = [A1_ascending[1][i].replace('mw A1 ascending ', '')]*len(ascending_pairs[i])
+
+ascending_pairs = pd.concat(ascending_pairs)
+ascending_pairs = ascending_pairs.reset_index(drop=True)
+
+VNC_layers = [[A1_MN]+us_MN, [A1_proprio]+ds_proprio, [A1_noci]+ds_noci, [A1_chordotonal]+ds_chord, [A1_classII_III]+ds_classII_III, [A1_external]+ds_external, [A1_unk]+ds_unk]
+A1_ascending = Celltype_Analyzer.get_skids_from_meta_annotation('mw A1 ascending', split=False)
+ascending_layers,ascending_skids = Celltype_Analyzer.layer_id(VNC_layers, general_names, A1_ascending)
+sens_asc_mat, sens_asc_mat_plotting = Promat.hop_matrix(ascending_skids.T, general_names, ascending_pairs.leftid, include_start=True)
+
+# only include 1 or 2-hop connections that are relevant
+sens_asc_mat_plotting[sens_asc_mat_plotting<(hops-1)]=0
+
+# remove coloring from 2-hop or greater connections to proprio (not relevant)
+sens_asc_mat_plotting.iloc[:, 0:4][sens_asc_mat_plotting.iloc[:, 0:4]==4]=0
+
+A00c_other_segments = [2511238, 2816457, 1042538]
+cols = [x for x in sens_asc_mat_plotting.columns if x not in A00c_other_segments]
+
+sort = [15315491, 14522404, 12998937, 8059283, 2123422, 10929797, 8644484, 7766016, 10949382, 5343578, 4595609, 3220616, 4206755, 4555763, 21250110, 18458734, 11455472, 3564452, 5606265, 8057753]
+
+fig, ax = plt.subplots(1,1)
+annots = sens_asc_mat.astype(int).astype(str)
+annots[annots=='0']=''
+sns.heatmap(sens_asc_mat_plotting.loc[['Proprio','Noci','Chord','ClassII_III','ES'], sort], annot=annots.loc[['Proprio','Noci','Chord','ClassII_III','ES'], sort], fmt='s', cmap='Blues', square=True, ax=ax)
+plt.savefig('plots/ascending_identity_plot.pdf', format='pdf', bbox_inches='tight')
+
+# %%
 
 # setting up volumes for future cells
 cns = pymaid.get_volume('cns')
@@ -111,9 +174,9 @@ def plot_pair(num, neurons, cns, neuropil, segments, view, method, neurons_prese
             navis.plot2d(segment, method=method, ax=ax)
         plt.show()
         if(neurons_present):
-            fig.savefig(f'VNC_interaction/plots/individual_asc_morpho/{num}_{neurons[0].skeleton_id}_morphology_{view}.png', dpi=200)
+            fig.savefig(f'plots/individual-asc-morpho_{num}-{neurons[0].skeleton_id}-morphology-{view}.png', dpi=200)
         if(neurons_present==False):
-            fig.savefig(f'VNC_interaction/plots/individual_asc_morpho/CNS_morphology_{view}.png', dpi=200)
+            fig.savefig(f'plots/individual-asc-morpho_CNS-morphology-{view}.png', dpi=200)
 
     if(view == 'front'):
         ax.azim = 90
@@ -123,9 +186,9 @@ def plot_pair(num, neurons, cns, neuropil, segments, view, method, neurons_prese
             navis.plot2d(segment, method=method, ax=ax)
         plt.show()
         if(neurons_present):
-            fig.savefig(f'VNC_interaction/plots/individual_asc_morpho/{num}_{neurons[0].skeleton_id}_morphology_{view}.png', dpi=200)
+            fig.savefig(f'plots/individual-asc-morpho_{num}-{neurons[0].skeleton_id}-morphology-{view}.png', dpi=200)
         if(neurons_present==False):
-            fig.savefig(f'VNC_interaction/plots/individual_asc_morpho/CNS_morphology_{view}.png', dpi=200)
+            fig.savefig(f'plots/individual-asc-morpho_CNS-morphology-{view}.png', dpi=200)
  
     if(view == 'top'):
         ax.elev=90
@@ -135,14 +198,12 @@ def plot_pair(num, neurons, cns, neuropil, segments, view, method, neurons_prese
             navis.plot2d(segment, method=method, ax=ax)
         plt.show()
         if(neurons_present):
-            fig.savefig(f'VNC_interaction/plots/individual_asc_morpho/{num}_{neurons[0].skeleton_id}_morphology_{view}.png', dpi=200)
+            fig.savefig(f'plots/individual-asc-morpho_{num}-{neurons[0].skeleton_id}-morphology-{view}.png', dpi=200)
         if(neurons_present==False):
-            fig.savefig(f'VNC_interaction/plots/individual_asc_morpho/CNS_morphology_{view}.png', dpi=200)
+            fig.savefig(f'plots/individual-asc-morpho_CNS-morphology-{view}.png', dpi=200)
  
-
-ascendings = [int(x) for x in sens_asc_mat_thresh.columns]
-asc_pairs = [pairs[pairs.leftid==x].loc[:, ['leftid', 'rightid']].values for x in ascendings]
-asc_pairs = [list(x) for sublist in asc_pairs for x in sublist]
+ascending_pairs = ascending_pairs.set_index('leftid', drop=False)
+asc_pairs = [list(x[0:2]) for x in ascending_pairs.loc[sort].values]
 
 segments = [T1_left, T1_right,
             T2_left, T2_right,
@@ -158,18 +219,19 @@ segments = [T1_left, T1_right,
 
 #neurons = pymaid.get_neurons(asc_pairs[0])
 #plot_pair(0, neurons, cns, neuropil, segments, 'side')
+method='3d_complex'
 
 for i in range(0, len(asc_pairs)):
     neurons = pymaid.get_neurons(asc_pairs[i])
-    plot_pair(i, neurons, cns, neuropil, segments, 'side')
-    
+    plot_pair(i, neurons, cns, neuropil, segments, 'side', method)
+'''
 for i in range(0, len(asc_pairs)):
     neurons = pymaid.get_neurons(asc_pairs[i])
-    plot_pair(i, neurons, cns, neuropil, segments, 'front')
-
+    plot_pair(i, neurons, cns, neuropil, segments, 'front', method)
+'''
 for i in range(0, len(asc_pairs)):
     neurons = pymaid.get_neurons(asc_pairs[i])
-    plot_pair(i, neurons, cns, neuropil, segments, 'top')
+    plot_pair(i, neurons, cns, neuropil, segments, 'top', method)
 
 segments = [SEZ_left, SEZ_right,
             T1_left, T1_right,
@@ -185,9 +247,9 @@ segments = [SEZ_left, SEZ_right,
             A8_left, A8_right]
 
 method='3d_complex'
-plot_pair('', [], cns, [], segments, 'side', method=method, neurons_present=False)
-plot_pair('', [], cns, [], segments, 'front', method=method,neurons_present=False)
-plot_pair('', [], cns, [], segments, 'top', method=method,neurons_present=False)
+#plot_pair('', [], cns, [], segments, 'side', method=method, neurons_present=False)
+#plot_pair('', [], cns, [], segments, 'front', method=method,neurons_present=False)
+#plot_pair('', [], cns, [], segments, 'top', method=method,neurons_present=False)
 
 # %%
 # plot dendritic synapses
