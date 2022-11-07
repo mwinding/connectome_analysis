ad2f40e172c78bdcd84880c18037d0d85af22630
Michael Winding <mwinding@alumni.nd.edu>
2022-11-07 10:55:40 +0000

    added removal of meta-meta annotations -rc

--- scripts/CATMAID_scripts/deannotate_using_meta_annotation.py
+++ scripts/CATMAID_scripts/deannotate_using_meta_annotation.py
@@ -9,16 +9,29 @@ import pymaid
 rm = pymaid.CatmaidInstance(url, token, name, password)
 
 # %%
-# deannotation all sub-annotations under meta-annotation
+# dennotate / remove single annotation
+
+annot = 'something'
+pymaid.remove_annotations(pymaid.get_skids_by_annotation(annot), annot)
+
+# %%
+# deannotate / remove all sub-annotations under meta-annotation
 ## VERY DANGEROUS; BE CAREFUL!!! ##
 
 meta = 'meta-something'
 annots = [annot for annot in pymaid.get_annotated(meta).name]
 
 [pymaid.remove_annotations(pymaid.get_skids_by_annotation(annot), annot) for annot in annots]
-# %%
-# dennotate single annotation
 
-annot = 'something'
-pymaid.remove_annotations(pymaid.get_skids_by_annotation(annot), annot)
+# %%
+# deannotate / remove annotations all sub- and sub-sub-anotations under meta-meta-annotation
+## EXTREMELY DANGEROUS; BE VERY CAREFUL!!!!!! ##
+
+meta_meta = 'meta-meta-something'
+meta_annots = [meta for meta in pymaid.get_annotated(meta_meta).name]
+annots = [[annot for annot in pymaid.get_annotated(meta).name] for meta in meta_annots]
+annots = [x for sublist in annots for x in sublist]
+
+[pymaid.remove_annotations(pymaid.get_skids_by_annotation(annot), annot) for annot in annots]
+
 # %%
