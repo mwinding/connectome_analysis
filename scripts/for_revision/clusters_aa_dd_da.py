2255d2e5c985214ed0178e9860ea5a6772e59394
Michael Winding <mwinding@alumni.nd.edu>
2022-11-07 12:35:49 +0000

    updated README.md

---
+++ scripts/for_revision/clusters_aa_dd_da.py
@@ -0,0 +1,22 @@
+# %%
+# identify clusters with higher than average aa, da, or dd connections (input or output)
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pymaid_creds import url, name, password, token
+from data_settings import data_date, pairs_path
+import pymaid
+rm = pymaid.CatmaidInstance(url, token, name, password)
+
+from contools import Celltype, Celltype_Analyzer, Promat
+
+# allows text to be editable in Illustrator
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+# font settings
+plt.rcParams['font.size'] = 5
+plt.rcParams['font.family'] = 'arial'
+
