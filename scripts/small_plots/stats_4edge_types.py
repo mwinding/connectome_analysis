4eafdd3ef698c20bb18a6ffd6bcbe4a9207c59a8
Michael Winding <mwinding@alumni.nd.edu>
2022-11-10 17:31:34 +0000

    updated to work with contools

--- scripts/small_plots/stats_4edge_types.py
+++ scripts/small_plots/stats_4edge_types.py
@@ -1,6 +1,7 @@
 #%%
 
 from pymaid_creds import url, name, password, token
+from data_settings import pairs_path, data_date
 import pymaid
 
 import matplotlib.pyplot as plt
@@ -8,8 +9,7 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import networkx as nx
-import connectome_tools.process_matrix as pm
-import connectome_tools.process_graph as pg
+from contools import Promat, Prograph
 
 # allows text to be editable in Illustrator
 plt.rcParams['pdf.fonttype'] = 42
@@ -21,17 +21,19 @@ plt.rcParams['font.family'] = 'arial'
 
 rm = pymaid.CatmaidInstance(url, token, name, password)
 
-brain = pymaid.get_skids_by_annotation('mw brain neurons')
-pairs = pm.Promat.get_pairs() # import pairs
+pairs = Promat.get_pairs(pairs_path=pairs_path) # import pairs
 
-Gad = nx.readwrite.graphml.read_graphml('data/graphs/Gad.graphml', node_type=int)
-Gaa = nx.readwrite.graphml.read_graphml('data/graphs/Gaa.graphml', node_type=int)
-Gdd = nx.readwrite.graphml.read_graphml('data/graphs/Gdd.graphml', node_type=int)
-Gda = nx.readwrite.graphml.read_graphml('data/graphs/Gda.graphml', node_type=int)
+Gad = nx.readwrite.graphml.read_graphml(f'data/processed/{data_date}/Gad.graphml', node_type=int)
+Gaa = nx.readwrite.graphml.read_graphml(f'data/processed/{data_date}/Gaa.graphml', node_type=int)
+Gdd = nx.readwrite.graphml.read_graphml(f'data/processed/{data_date}/Gdd.graphml', node_type=int)
+Gda = nx.readwrite.graphml.read_graphml(f'data/processed/{data_date}/Gda.graphml', node_type=int)
 
 G_types = [Gad, Gaa, Gdd, Gda]
 G_names = ['Axon-Dendrite', 'Axon-Axon', 'Dendrite-Dendrite', 'Dendrite-Axon']
-brain = pymaid.get_skids_by_annotation('mw brain paper clustered neurons')
+
+brain = pymaid.get_skids_by_annotation(['mw brain and inputs', 'mw brain accessory neurons'])
+remove_neurons = pymaid.get_skids_by_annotation(['mw brain very incomplete', 'mw partially differentiated', 'mw motor'])
+brain = list(np.setdiff1d(brain, remove_neurons)) # remove neurons that are incomplete or partially differentiated (as well as SEZ motor neurons)
 
 # include only brain inputs, interneurons, and outputs
 G_types = [G.subgraph(np.intersect1d(list(G.nodes), brain)) for G in G_types]
@@ -49,7 +51,7 @@ for node_count in node_counts:
 edge_counts = [len(G.edges) for G in G_types]
 all_possible_edges = [len(G.nodes)**2 for G in G_types]
 density = [edge_count/all_possible*100 for edge_count, all_possible in zip(edge_counts, all_possible_edges)]
-density = [f'{G_names[i]}: {round(density[i], 2)}%' for i in range(0, len(density))]
+density = [f'{G_names[i]}: {density[i]:.2f}%' for i in range(0, len(density))]
 
 print('\n** Density **')
 for dens in density:
@@ -79,7 +81,7 @@ total_edges = sum(edge_counts)
 fraction_edges = [edge_count/total for edge_count, total in zip(edge_counts, [total_edges]*len(edge_counts))]
 fraction_edges_label = [f'{round(fraction_edges[i]*100)}%' for i in range(0, len(fraction_edges))]
 
-synapse_counts = [list(pg.Prograph.path_edge_attributes(G, edge, 'weight')[0]) + [G_names[i]] for i, G in enumerate(G_types) for edge in list(G.edges)]
+synapse_counts = [list(Prograph.path_edge_attributes(G, edge, 'weight')[0]) + [G_names[i]] for i, G in enumerate(G_types) for edge in list(G.edges)]
 synapse_counts = pd.DataFrame(synapse_counts, columns = ['pre', 'post', 'weight', 'type'])
 synapse_counts = synapse_counts.groupby('type').sum().iloc[[1,0,3,2], :] #sum types and reorder
 fraction_synapses = synapse_counts.weight/sum(synapse_counts.weight)
@@ -95,7 +97,7 @@ ax = axs[1]
 ax.set_axis_off()
 ax.set(title='Synapse Types')
 squarify.plot(fraction_synapses, label = fraction_synapses_label, color=color, alpha=alpha, text_kwargs={'color':'white'}, ax=ax)
-plt.savefig('small_plots/plots/treemap_fraction-edges_fraction-synapses.pdf', format='pdf', bbox_inches='tight')
+plt.savefig('plots/treemap_fraction-edges_fraction-synapses.pdf', format='pdf', bbox_inches='tight')
 
 
 fig, axs = plt.subplots(1,2, figsize=(2.5,1))
@@ -108,7 +110,7 @@ ax = axs[1]
 ax.set_axis_off()
 ax.set(title='Synapse Types')
 squarify.plot(fraction_synapses, label = [f'{round(x/1000)}k' for x in synapse_counts.weight], color=color, alpha=alpha, text_kwargs={'color':'white'}, ax=ax)
-plt.savefig('small_plots/plots/treemap_count-edges_count-synapses.pdf', format='pdf', bbox_inches='tight')
+plt.savefig('plots/treemap_count-edges_count-synapses.pdf', format='pdf', bbox_inches='tight')
 # %%
 # counts for edge width in figure
 
