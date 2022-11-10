f672e8d4149b92ae4de200233f7d0496f6c7aff7
Michael Winding <mwinding@alumni.nd.edu>
2022-11-10 17:31:24 +0000

    updated with new data

--- scripts/small_plots/feedforward-feedback_signalflow.py
+++ scripts/small_plots/feedforward-feedback_signalflow.py
@@ -13,8 +13,9 @@ plt.rcParams['ps.fonttype'] = 42
 plt.rcParams['font.size'] = 5
 plt.rcParams['font.family'] = 'arial'
 
-#data = [[.2, .8], [.47, .53], [.42, .58], [.62, .38]] #old data
-data = [[.23, .77], [.46, .54], [.36, .64], [.67, .33]]
+# data collected from Benjamin Pedigo's plot in S57
+#data = [[.23, .77], [.46, .54], [.36, .64], [.67, .33]]
+data = [[.13, .87], [.41, .59], [.31, .69], [.71, .29]]
 df = pd.DataFrame(data, index=['a-d', 'a-a', 'd-d', 'd-a'], columns = ['feedback', 'feedforward'])
 df['bias'] = df.feedforward-df.feedback
 
@@ -22,37 +23,35 @@ fig, ax = plt.subplots(1,1,figsize=(0.75,2))
 plt.bar(x = range(len(df.index)), height = df.bias)
 #plt.bar(x = range(len(df.index)), height = df.feedback, bottom=df.feedforward)
 ax.set(ylim=(-1,1))
-plt.savefig('small_plots/plots/ff-fb_signal-flow-plots.pdf', format='pdf', bbox_inches='tight')
+plt.savefig('plots/ff-fb_signal-flow-plots.pdf', format='pdf', bbox_inches='tight', ax=ax)
 
 fig, ax = plt.subplots(1,1,figsize=(0.75,2))
 plt.bar(x = range(len(df.index)), height = df.feedforward)
 plt.bar(x = range(len(df.index)), height = -df.feedback)
 ax.set(ylim=(-1,1))
-plt.savefig('small_plots/plots/ff-fb_signal-flow-plots-type2.pdf', format='pdf', bbox_inches='tight')
-
-fig, ax = plt.subplots(1,1,figsize=(0.75,2))
+plt.savefig('plots/ff-fb_signal-flow-plots-type2.pdf', format='pdf', bbox_inches='tight', ax=ax)
 
 # %%
 # seaborn barplots
 
 '''
 # old data
-data = [[.8, 'feedforward', 'a-d'], [.2, 'feedback', 'a-d'], 
-        [.53, 'feedforward', 'a-a'], [.47, 'feedback', 'a-a'], 
-        [.58, 'feedforward', 'd-d'], [.42, 'feedback', 'd-d'], 
-        [.38, 'feedforward', 'd-a'], [.62, 'feedback', 'd-a']]
-'''
 data = [[.77, 'feedforward', 'a-d'], [.23, 'feedback', 'a-d'], 
         [.54, 'feedforward', 'a-a'], [.46, 'feedback', 'a-a'], 
         [.64, 'feedforward', 'd-d'], [.36, 'feedback', 'd-d'], 
         [.33, 'feedforward', 'd-a'], [.67, 'feedback', 'd-a']]
+'''
+data = [[.87, 'feedforward', 'a-d'], [.13, 'feedback', 'a-d'], 
+        [.59, 'feedforward', 'a-a'], [.41, 'feedback', 'a-a'], 
+        [.69, 'feedforward', 'd-d'], [.31, 'feedback', 'd-d'], 
+        [.29, 'feedforward', 'd-a'], [.71, 'feedback', 'd-a']]
 
 df = pd.DataFrame(data, columns = ['fraction', 'direction', 'edge_type'])
 
 fig, ax = plt.subplots(1,1,figsize=(1.5,1))
 sns.barplot(data=df, x='edge_type', y='fraction', hue='direction')
 ax.set(ylim=(0,1))
-plt.savefig('small_plots/plots/ff-fb_signal-flow-plots-type3.pdf', format='pdf', bbox_inches='tight')
+plt.savefig('plots/ff-fb_signal-flow-plots-type3.pdf', format='pdf', bbox_inches='tight')
 
 
 # %%
