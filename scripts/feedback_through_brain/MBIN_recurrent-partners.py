5d33e793b05ea97c00eeef1388c12b3a2728ba38
Michael Winding <mwinding@alumni.nd.edu>
2022-12-16 13:10:12 +0000

    updated and remade some plots

--- scripts/feedback_through_brain/MBIN_recurrent-partners.py
+++ scripts/feedback_through_brain/MBIN_recurrent-partners.py
@@ -66,19 +66,19 @@ def identify_recurrent_partners(partners_df, hops):
 
     return(recurrent_partners)
 
-partners_df = cascades_df_with_inputs.loc[:, ['ds_partners_5hop']]
+partners_df = cascades_df_with_inputs.loc[:, [f'ds_partners_{hops}hop']]
 
 # convert to pair_ids; did this as a stop-gap because expanding the recurrent partners took too long on my current compute
-partners_df['ds_partners_5hop'] = [list(np.intersect1d(x, partners_df.index)) for x in partners_df.ds_partners_5hop]
+partners_df[f'ds_partners_{hops}hop'] = [list(np.intersect1d(x, partners_df.index)) for x in partners_df.loc[:, f'ds_partners_{hops}hop']]
 
 # identify upstream neurons, takes a few minutes
-partners_df['us_partners_5hop'] = generate_us_df(partners_df, hops=5)
+partners_df[f'us_partners_{hops}hop'] = generate_us_df(partners_df, hops=hops)
 
 # identify recurrent partners
-partners_df['recurrent_partners_5hop'] = identify_recurrent_partners(partners_df, hops=5)
+partners_df[f'recurrent_partners_{hops}hop'] = identify_recurrent_partners(partners_df, hops=hops)
 
 # fraction of recurrent vs. nonrecurrent parents at 5-hop
-hops = 5
+hops = 8
 frac_recurrent = [len(partners_df.loc[i, f'recurrent_partners_{hops}hop'])/len(partners_df.loc[i, f'ds_partners_{hops}hop']) if len(partners_df.loc[i, f'ds_partners_{hops}hop'])>0 else 0 for i in partners_df.index]
 partners_df[f'fraction_recurrent_{hops}hop'] = frac_recurrent
 partners_df[f'fraction_nonrecurrent_{hops}hop'] = 1-partners_df.loc[:, f'fraction_recurrent_{hops}hop']
@@ -92,33 +92,33 @@ MBIN_order = [15592096, 3886356, 7901791, 4381377, 7057894, 4414163, 12871993, 1
 MBIN_partners = partners_df.loc[MBIN_order].copy()
 
 # add back left/right neurons for recurrent_partners_5hop (currently pair_ids)
-MBIN_partners.loc[:, 'recurrent_partners_5hop'] = [Promat.get_paired_skids(skids, pairs, unlist=True) for skids in list(MBIN_partners.recurrent_partners_5hop)]
+MBIN_partners.loc[:, f'recurrent_partners_{hops}hop'] = [Promat.get_paired_skids(skids, pairs, unlist=True) for skids in list(MBIN_partners.loc[:, f'recurrent_partners_{hops}hop'])]
 
-MBIN_rec_partner_cts = [Celltype(f'{MBIN_partners.index[i]}_recurrent-partners', skids) for i, skids in enumerate(list(MBIN_partners.recurrent_partners_5hop))]
+MBIN_rec_partner_cts = [Celltype(f'{MBIN_partners.index[i]}_recurrent-partners', skids) for i, skids in enumerate(list(MBIN_partners.loc[:, f'recurrent_partners_{hops}hop']))]
 MBIN_rec_partner_cta = Celltype_Analyzer(MBIN_rec_partner_cts)
 
 _, celltypes = Celltype_Analyzer.default_celltypes()
 MBIN_rec_partner_cta.set_known_types(celltypes)
 MBIN_rec_partner_cta.memberships()
-MBIN_rec_partner_cta.plot_memberships(path='plots/MBIN_recurrent-partners-5hops.pdf', figsize=(3,1))
-MBIN_rec_partner_cta.plot_memberships(path='plots/MBIN_recurrent-partners-5hops_raw-num.pdf', figsize=(3,1), raw_num=True)
+MBIN_rec_partner_cta.plot_memberships(path=f'plots/MBIN_recurrent-partners-{hops}hops.pdf', figsize=(3,1))
+MBIN_rec_partner_cta.plot_memberships(path=f'plots/MBIN_recurrent-partners-{hops}hops_raw-num.pdf', figsize=(3,1), raw_num=True)
 
 # %%
-# how many sensory neurons from each modality talk to MBINs within 5-hops?
+# how many sensory neurons from each modality talk to MBINs within X-hops?
 
 sens_order = ['olfactory', 'gustatory-external', 'gustatory-pharyngeal', 'enteric', 'thermo-warm', 'thermo-cold', 'visual', 'noci', 'mechano-Ch', 'mechano-II/III', 'proprio', 'respiratory']
 sens_color = ['#17A55C', '#8DC048', '#D0D823', '#DBA933', '#E92930', '#3BA6D3', '#5C62AC', '#E05E35', '#007D70', '#48B191', '#DF4F9B', '#652D90']
 sens_modalities = [Celltype(name, Celltype_Analyzer.get_skids_from_meta_annotation(f'mw {name}'), sens_color[i]) for i, name in enumerate(sens_order)]
 
 # add back left/right neurons for us_partners_5hop (currently pair_ids)
-MBIN_partners.loc[:, 'us_partners_5hop'] = [Promat.get_paired_skids(skids, pairs, unlist=True) for skids in list(MBIN_partners.us_partners_5hop)]
+MBIN_partners.loc[:, f'us_partners_{hops}hop'] = [Promat.get_paired_skids(skids, pairs, unlist=True) for skids in list(MBIN_partners.loc[:, f'us_partners_{hops}hop'])]
 
-MBIN_us_partner_cts = [Celltype(f'{MBIN_partners.index[i]}_us-partners', skids) for i, skids in enumerate(list(MBIN_partners.us_partners_5hop))]
+MBIN_us_partner_cts = [Celltype(f'{MBIN_partners.index[i]}_us-partners', skids) for i, skids in enumerate(list(MBIN_partners.loc[:, f'us_partners_{hops}hop']))]
 MBIN_us_partner_cta = Celltype_Analyzer(MBIN_us_partner_cts)
 MBIN_us_partner_cta.set_known_types(sens_modalities, unknown=False)
 MBIN_us_partner_cta.memberships()
-MBIN_us_partner_cta.plot_memberships(path='plots/MBIN_us-sens-partners-5hops.pdf', figsize=(3,1))
-MBIN_us_partner_cta.plot_memberships(path='plots/MBIN_us-sens-partners-5hops_raw-num.pdf', figsize=(3,1), raw_num=True)
+MBIN_us_partner_cta.plot_memberships(path=f'plots/MBIN_us-sens-partners-{hops}hops.pdf', figsize=(3,1))
+MBIN_us_partner_cta.plot_memberships(path=f'plots/MBIN_us-sens-partners-{hops}hops_raw-num.pdf', figsize=(3,1), raw_num=True)
 
 # convert to fraction of each sensory modality
 fraction_sens = MBIN_us_partner_cta.memberships(raw_num=True)
@@ -167,6 +167,7 @@ ds_partners_5hop = Parallel(n_jobs=-1)(delayed(Cascade_Analyzer.pairwise_thresho
 
 # %%
 # make heatmap of sensory modalities to DANs
+# what is actually used in the paper
 
 hops = 5
 
@@ -176,3 +177,35 @@ MBINs_sens_input = pd.DataFrame(MBINs_sens_input, index=sens_order, columns = MB
 fig,ax = plt.subplots(figsize=(2,2))
 sns.heatmap(MBINs_sens_input, square=True, ax=ax, vmin=0)
 plt.savefig('plots/MBIN-input_casc-sens-modality.pdf', format='pdf', bbox_inches='tight')
+
+(MBINs_sens_input>=(n_init/2)).sum(axis=0)
+
+# %%
+# cascades from sensory modalities (all neurons at once)
+
+# load cascades from pickle object; generated in generate_data/cascades_all-modalities.py
+n_init = 1000
+all_data_df = pickle.load(open(f'data/cascades/all-sensory-modalities_processed-cascades_{n_init}-n_init_{data_date}.p', 'rb'))
+
+# load pairs
+pairs = Promat.get_pairs(pairs_path=pairs_path)
+
+# find neurons downstream of each modality
+threshold = n_init/2
+hops = 8
+ds_partners_5hop = Parallel(n_jobs=-1)(delayed(Cascade_Analyzer.pairwise_threshold)(hh_pairwise=all_data_df.iloc[i, 1].hh_pairwise, pairs=pairs, threshold=threshold, hops=hops) for i in tqdm(range(len(all_data_df.index))))
+
+# %%
+# make heatmap of sensory modalities to DANs
+
+hops = 8
+
+MBINs_sens_input = [list(all_data_df.cascade_objs[i].hh_pairwise.loc[(slice(None), MBIN_order), 0:hops].sum(axis=1)) for i in range(len(all_data_df.cascade_objs))]
+MBINs_sens_input = pd.DataFrame(MBINs_sens_input, index=sens_order, columns = MBIN_order)
+
+fig,ax = plt.subplots(figsize=(2,2))
+sns.heatmap(MBINs_sens_input, square=True, ax=ax, vmin=0)
+plt.savefig('plots/MBIN-input_casc-sens-modality.pdf', format='pdf', bbox_inches='tight')
+
+(MBINs_sens_input>=(n_init/2)).sum(axis=0)
+# %%
