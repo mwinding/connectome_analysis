2cd6ea1995a9e0548cae5447f74ffda956c8ad24
Michael Winding <mwinding@alumni.nd.edu>
2022-12-22 16:36:37 +0000

    delete files

--- data_settings.py
+++
@@ -1,4 +0,0 @@
-pairs_path = 'data/pairs/pairs-2022-02-14.csv'
-data_date = '2022-11-03' # dataset of brain and inputs
-data_date_A1_brain = '2022-11-03b' # dataset of brain, brain inputs, A1, and A1 inputs
-data_date_projectome = '2022-05-05'
