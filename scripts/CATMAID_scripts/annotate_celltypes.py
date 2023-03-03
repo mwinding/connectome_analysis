a59188c10e9a7329677cd6898edef4050de8f919
Michael Winding <mwinding@alumni.nd.edu>
2023-03-03 14:54:00 +0000

    updated annotations

--- scripts/CATMAID_scripts/annotate_celltypes.py
+++ scripts/CATMAID_scripts/annotate_celltypes.py
@@ -8,13 +8,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
-import connectome_tools.process_matrix as pm
-import connectome_tools.celltype as ct
+from contools import Celltype, Celltype_Analyzer, Promat
 import navis
 
-celltypes_df, celltypes = ct.Celltype_Analyzer.default_celltypes()
-celltypes_df.iloc[6, 0] = 'MB-FFNs'
-celltypes[6].name = 'MB-FFNs'
+celltypes_df, celltypes = Celltype_Analyzer.default_celltypes()
+
+# old names and new replacement names
+replace_dict = {'FFNs': 'MB-FFNs',
+                'pre-dSEZs': 'pre-DN-SEZs',
+                'pre-dVNCs': 'pre-DN-VNCs',
+                'dSEZs': 'DN-SEZs',
+                'dVNCs': 'DN-VNCs'}
+
+# replace names with updated ones in dataFrame
+for i in range(len(celltypes_df.index)):
+    name = celltypes_df.iloc[i, 0]
+    if (name in replace_dict.keys()):
+        celltypes_df.iloc[i, 0] = replace_dict[celltypes_df.iloc[i, 0]]
+
+# replace names with updated ones in Celltype objects
+for i in range(len(celltypes)):
+    name = celltypes[i].name
+    if (name in replace_dict.keys()):
+        celltypes[i].name = replace_dict[celltypes[i].name]  
 
 # %%
 # write the binary celltypes
@@ -23,8 +39,8 @@ celltypes[6].name = 'MB-FFNs'
 pymaid.add_meta_annotations([f'ctd {x.name}' for x in celltypes], 'mw brain celltypes discrete')
 
 pymaid.clear_cache()
-other = list(np.setdiff1d(pymaid.get_skids_by_annotation('mw brain neurons'),
-                        ct.Celltype_Analyzer.get_skids_from_meta_annotation('mw brain celltypes discrete')))
+brain_neurons = np.setdiff1d(pymaid.get_skids_by_annotation('mw brain neurons'), pymaid.get_skids_by_annotation(['mw brain very incomplete', 'mw partially differentiated']))
+other = np.setdiff1d(brain_neurons, Celltype_Analyzer.get_skids_from_meta_annotation('mw brain celltypes discrete'))
 
 pymaid.add_annotations(other, 'ctd other')
 pymaid.add_meta_annotations('ctd other', 'mw brain celltypes discrete')
@@ -34,16 +50,20 @@ pymaid.add_meta_annotations('ctd other', 'mw brain celltypes discrete')
 
 celltype_names = ['sensories', 'PNs', 'ascendings', 'PNs-somato', 'LNs', 'LHNs', 'FFNs', 'MBINs', 'KCs', 'MBONs', 'MB-FBNs', 'CNs', 'pre-dSEZs', 'pre-dVNCs', 'RGNs', 'dSEZs', 'dVNCs']
 annots = ['mw brain ' + x for x in celltype_names]
-celltype_names = ['sensories', 'PNs', 'ascendings', 'PNs-somato', 'LNs', 'LHNs', 'MB-FFNs', 'MBINs', 'KCs', 'MBONs', 'MB-FBNs', 'CNs', 'pre-dSEZs', 'pre-dVNCs', 'RGNs', 'dSEZs', 'dVNCs']
 
-all_celltypes = [ct.Celltype(celltype_names[i], ct.Celltype_Analyzer.get_skids_from_meta_annotation(annot)) for i, annot in enumerate(annots)]
+for i in range(len(celltype_names)):
+    name = celltype_names[i]
+    if (name in replace_dict.keys()):
+        celltype_names[i] = replace_dict[celltype_names[i]]     
+
+all_celltypes = [Celltype(celltype_names[i], Celltype_Analyzer.get_skids_from_meta_annotation(annot)) for i, annot in enumerate(annots)]
 
 [pymaid.add_annotations(x.skids, f'ct {x.name}') for x in all_celltypes]
 pymaid.add_meta_annotations([f'ct {x.name}' for x in celltypes], 'mw brain celltypes')
 
 pymaid.clear_cache()
-other = list(np.setdiff1d(pymaid.get_skids_by_annotation('mw brain neurons'),
-                        ct.Celltype_Analyzer.get_skids_from_meta_annotation('mw brain celltypes')))
+brain_neurons = np.setdiff1d(pymaid.get_skids_by_annotation('mw brain neurons'), pymaid.get_skids_by_annotation(['mw brain very incomplete', 'mw partially differentiated']))
+other = list(np.setdiff1d(brain_neurons, Celltype_Analyzer.get_skids_from_meta_annotation('mw brain celltypes')))
 
 pymaid.add_annotations(other, 'ct other')
 pymaid.add_meta_annotations('ct other', 'mw brain celltypes')
